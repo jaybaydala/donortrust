e97ae3ace9d0ca726991f5b6eb6b2b926cb5cdff
pduby <pduby@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-03-18 23:00:05 +0000

    Default (generatated) RESTful unit tests for Status and Milestone with history

---
+++ test/unit/milestone_history_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class MilestoneHistoryTest < Test::Unit::TestCase
+  fixtures :milestone_histories
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
