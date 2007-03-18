45fd19716d97eeba9e74398c6040f9572987e997
pduby <pduby@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-03-18 22:51:35 +0000

    Merge Status, Milestone and MilestoneHistory table creation

---
+++ db/migrate/001_donortrust_rel_001.rb
@@ -0,0 +1,69 @@
+class DonortrustRel001 < ActiveRecord::Migration
+  def self.up
+    create_table :projects do |t|
+      t.column :program_id, :integer
+      t.column :category_id, :integer
+      t.column :name, :string, :null => false
+      t.column :description, :text
+      t.column :cost, :float
+      t.column :dollars_spent, :float
+      t.column :expected_completion_date, :datetime
+      t.column :start_date, :datetime
+      t.column :end_date, :datetime
+      t.column :status_id, :integer
+      t.column :contact_id, :integer
+      t.column :location_id, :integer
+    end #:projects
+
+    create_table :project_histories do |t|
+      t.column :project_id, :integer, :null => false
+      t.column :date, :datetime
+      t.column :cost, :float
+      t.column :dollars_spent, :float
+      t.column :expected_completion_date, :datetime      
+      t.column :start_date, :datetime
+      t.column :end_date, :datetime
+      t.column :user_id, :integer  
+      t.column :status_id, :integer
+    end #:project_histories
+
+    create_table :milestone_histories do |t|
+      t.column :milestone_id, :int
+      t.column :created_at, :datetime
+      t.column :reason, :text
+
+      t.column :category, :string
+      t.column :description, :text
+      t.column :start, :date
+      t.column :end, :date
+      t.column :status_id, :int
+    end #:milestone_histories
+
+    create_table :milestones do |t|
+      t.column :category, :string, :null => false
+      t.column :description, :text, :null => false
+      t.column :start, :date, :null => false
+      t.column :end, :date, :null => false
+      t.column :status_id, :int, :null => false
+    end #:milestones
+
+    create_table :statuses do |t|
+      t.column :category, :string, :null => false
+      t.column :description, :text, :null => false
+    end #:statuses
+
+    # Load some initial data
+    # Rails 'convention' is to put the fixture files in test\fixtures
+    # Do we really want to override that?
+    directory = File.join(File.dirname(__FILE__), "dev_data")
+    #Fixtures.create_fixtures(directory, "projects")
+  end # self.up
+
+  def self.down
+    drop_table :projects
+    drop_table :project_histories
+    drop_table :milestones
+    drop_table :milestone_histories
+    drop_table :statuses
+  end # self.down
+end #class DonorTrustRel001 
