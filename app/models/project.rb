1fd09a1845f859f9c4aab1c128c68ea4e7503bd3
tadatoshi <tadatoshi@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-03-21 19:00:17 +0000

    Added pre-populating data with the data from project. Based on Milestone implementation.

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,23 @@ class Project < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name  
 
+  def save_with_audit
+  
+    save_result = false
+    
+    if( self.save )
+      project_history                          = ProjectHistory.new(:project_id => self.id)
+      project_history.expected_completion_date = self.expected_completion_date
+      project_history.status_id                = self.status_id
+      save_result                              = project_history.save
+    end
+    
+    return save_result
+    
+  end
+  
+  def self.is_a_project?( object )
+    return object.class.to_s == "Project"
+  end  
+
 end
--- app/models/project_history.rb
+++ app/models/project_history.rb
@@ -11,5 +11,13 @@ class ProjectHistory < ActiveRecord::Base
       errors.add_to_base("Project with id=#{self.project_id} doesn't exist.")
     end
   end
-
+  
+  def self.new_audit( project )
+    raise ArgumentError, "need a Project, not a '#{project.class.to_s}'" if not Project.is_a_project?( project )
+    project_history                          = ProjectHistory.new(:project_id => project.id)
+    project_history.expected_completion_date = project.expected_completion_date
+    project_history.status_id                = project.status_id
+    return project_history
+  end
+  
 end
