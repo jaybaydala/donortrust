06b203ac0a5ead241bbf8e1b659162cd1bea004f
pduby <pduby@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-04-08 20:25:55 +0000

    Backport some changes to make some generic code easier to clone in the future

--- test/unit/milestone_category_test.rb
+++ test/unit/milestone_category_test.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 class MilestoneCategoryTest < Test::Unit::TestCase
   fixtures :milestone_categories
 
-  def clean_new_milestone_category( overrides = {})
+  def clean_new_instance( overrides = {})
     # Build (and return) an instance starting from known (expected) valid attribute
     # values, processing overides for any/all specified attributes
     opts = {
@@ -14,25 +14,25 @@ class MilestoneCategoryTest < Test::Unit::TestCase
     # :id is a protected attribute.  Must set after creating instance
   end
 
-  def test_clean_category
+  def test_clean_instance
     # Should be valid to create a new instance from the 'clean' instance created
     # by the helper function.
-    assert_valid clean_new_milestone_category
+    assert_valid clean_new_instance
   end
 
   def test_create_with_empty_category
     # Should not be valid to create a new instance with an empty or blank category
-    assert_invalid( clean_new_milestone_category, :category, nil, "" )
+    assert_invalid( clean_new_instance, :category, nil, "" )
   end
   
   def test_create_with_empty_description
     # Should not be valid to create a new instance with an empty or blank description
-    assert_invalid( clean_new_milestone_category, :description, nil, "" )
+    assert_invalid( clean_new_instance, :description, nil, "" )
   end
   
   def test_unique_create_category
     # Should not be valid to reuse an existing category to create a new instance
-    assert_invalid( clean_new_milestone_category, :category, milestone_categories( :testone ).category )
+    assert_invalid( clean_new_instance, :category, milestone_categories( :testone ).category )
   end
   
   def test_edit_to_empty_category
--- test/unit/milestone_status_test.rb
+++ test/unit/milestone_status_test.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 class MilestoneStatusTest < Test::Unit::TestCase
   fixtures :milestone_statuses
 
-  def clean_new_milestone_status( overrides = {})
+  def clean_new_instance( overrides = {})
     # Build (and return) an instance starting from known (expected) valid attribute
     # values, processing overides for any/all specified attributes
     opts = {
@@ -14,25 +14,25 @@ class MilestoneStatusTest < Test::Unit::TestCase
     # :id is a protected attribute.  Must set after creating instance
   end
 
-  def test_clean_status
+  def test_clean_instance
     # Should be valid to create a new instance from the 'clean' instance created
     # by the helper function.
-    assert_valid clean_new_milestone_status
+    assert_valid clean_new_instance
   end
 
   def test_create_with_empty_status
     # Should not be valid to create a new instance with an empty or blank status
-    assert_invalid( clean_new_milestone_status, :status, nil, "" )
+    assert_invalid( clean_new_instance, :status, nil, "" )
   end
   
   def test_create_with_empty_description
     # Should not be valid to create a new instance with an empty or blank description
-    assert_invalid( clean_new_milestone_status, :description, nil, "" )
+    assert_invalid( clean_new_instance, :description, nil, "" )
   end
   
   def test_unique_create_status
     # Should not be valid to reuse an existing status to create a new instance
-    assert_invalid( clean_new_milestone_status, :status, milestone_statuses( :proposed ).status )
+    assert_invalid( clean_new_instance, :status, milestone_statuses( :proposed ).status )
   end
   
   def test_edit_to_empty_status
