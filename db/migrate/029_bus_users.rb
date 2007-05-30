81377f69c449e1471135d501fe398f706f13f452
joshmm1 <joshmm1@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-05-30 22:38:12 +0000

    Opps... removed something broken
    -josh

--- db/migrate/029_bus_users.rb
+++ db/migrate/029_bus_users.rb
@@ -10,11 +10,7 @@ class BusUsers < ActiveRecord::Migration
       t.column :remember_token,              :string
       t.column :remember_token_expires_at,   :datetime
     end
-    
-     if (ENV['RAILS_ENV'] = 'development')
-      directory = File.join(File.dirname(__FILE__), "dev_data")
-      Fixtures.create_fixtures(directory, "bus_users")
-    end
+   
   end
 
   def self.down
