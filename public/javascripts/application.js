72269d352a4139c884581e10ca92928bcd5b7a74
jdoree <jdoree@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-06-01 14:23:41 +0000

    Added function for redirecting to a page based on selected item in a forms combo box.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function formLinkRedirector(form){
+    var targetUrl = form.site.options[form.site.selectedIndex].value;
+    window.location.href = targetUrl;
+}
