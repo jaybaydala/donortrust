f2f00d0bbfb8017ee31708e93f751cb259b360da
pduby <pduby@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-06-08 05:01:51 +0000

    add fixtures for development users

--- db/migrate/030_bus_user_types.rb
+++ db/migrate/030_bus_user_types.rb
@@ -4,6 +4,11 @@ class BusUserTypes < ActiveRecord::Migration
       t.column :name,                      :string
       t.column :security_level_id,         :int
     end    
+
+    if (ENV['RAILS_ENV'] = 'development')
+      directory = File.join(File.dirname(__FILE__), "dev_data")
+      Fixtures.create_fixtures(directory, "bus_user_types")
+    end
   end
 
   def self.down
--- db/migrate/031_bus_security_levels.rb
+++ db/migrate/031_bus_security_levels.rb
@@ -4,6 +4,11 @@ class BusSecurityLevels < ActiveRecord::Migration
     create_table :bus_security_levels, :force => true do |t|
       t.column :level,                     :int
     end
+
+    if (ENV['RAILS_ENV'] = 'development')
+      directory = File.join(File.dirname(__FILE__), "dev_data")
+      Fixtures.create_fixtures(directory, "bus_security_levels")
+    end
   end
 
   def self.down
