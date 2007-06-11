e328fdb02f71c5193d4ad356037ccb2e75833b22
joshmm1 <joshmm1@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-06-11 04:32:18 +0000

    Increasing security functionality.  Now bus_user has a type, actions, based on controllers.
    -josh

---
+++ test/unit/bus_security_level_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class BusAdmin::BusSecurityLevelTest < Test::Unit::TestCase
+  fixtures :bus_admin_bus_security_levels
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
---
+++ test/unit/bus_user_type_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class BusAdmin::BusUserTypeTest < Test::Unit::TestCase
+  fixtures :bus_admin_bus_user_types
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
