d4babbae22074a75e601ae71d8db554e08089e54
jfurtner <jfurtner@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-06-17 00:50:11 +0000

    renaming partner_histories to partner_versions

--- db/migrate/004_partner_versions_rel_001.rb
+++ db/migrate/004_partner_versions_rel_001.rb
@@ -1,6 +1,6 @@
 require 'active_record/fixtures'
 
-class PartnerHistoriesRel001 < ActiveRecord::Migration
+class PartnerVersionsRel001 < ActiveRecord::Migration
   def self.up
     #    if (ENV['RAILS_ENV'] == 'development')
     ##      directory = File.join(File.dirname(__FILE__), "dev_data")
