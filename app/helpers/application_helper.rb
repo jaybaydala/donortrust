d0c4dc6931640952b106ac1674698ae0b8e414a7
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-06-26 21:30:42 +0000

    Added some dt_-specific helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,22 @@
 module ApplicationHelper
   #include AuthenticatedSystem
 
+  def dt_head
+    render 'dt/shared/head'
+  end
+
+  def dt_nav
+    render 'dt/shared/nav'
+  end
+
+  def dt_masthead
+    @masthead = { 
+      :image => '/images/dt/masthead_default.jpg'
+      }
+    render 'dt/shared/masthead'
+  end
+
+  def dt_project_search
+    render 'dt/shared/project_search'
+  end
 end
