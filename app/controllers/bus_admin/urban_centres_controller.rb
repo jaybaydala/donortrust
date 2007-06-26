7da85cee972dfc8bef61deb3f5cd3cc12160d2c0
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-06-26 20:21:17 +0000

    Update projects and urban_centres_controllers to reflect urban_centre.name rather than urban_centre.urban_centre_name

--- app/controllers/bus_admin/urban_centres_controller.rb
+++ app/controllers/bus_admin/urban_centres_controller.rb
@@ -1,8 +1,8 @@
 class BusAdmin::UrbanCentresController < ApplicationController
   before_filter :login_required
   active_scaffold :urban_centres do |config|
-    config.columns = [:urban_centre_name, :blog_name, :rss_url, :population, :village_plan, :region, :facebook_group_id]
-    config.columns[ :urban_centre_name ].label = "Name"
+    config.columns = [:name, :blog_name, :rss_url, :population, :village_plan, :region, :facebook_group_id]
+    config.columns[ :name ].label = "Name"
   end
 
   
