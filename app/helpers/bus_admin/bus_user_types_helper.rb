186bd02019efb1637fd6483964d6e05b477beea9
joshmm1 <joshmm1@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-07-09 20:17:28 +0000

    Bus Security additions
    -josh

--- app/helpers/bus_admin/bus_user_types_helper.rb
+++ app/helpers/bus_admin/bus_user_types_helper.rb
@@ -1,8 +1,11 @@
 module BusAdmin::BusUserTypesHelper
 
+  
 #  def bus_secure_actions_form_column(record, input_name)
-#    check_box :record, :bus_secure_actions, :name => input_name
+#        puts "Record = " + record.bus_secure_actions.id.to_s + " input_name = " + input_name
+#        
+#        render :partial => 'bus_admin/bus_account/controller_actions'  
+#       
+#        
 #  end
-
-
 end
---
+++ app/helpers/bus_admin/bus_users_helper.rb
@@ -0,0 +1,2 @@
+module BusAdmin::BusUsersHelper
+end
