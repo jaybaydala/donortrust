f4910b7c555b4f706a0bc53ceebbf044dd6f152e
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-07-19 21:20:42 +0000

    add in File.exists? check for dev_data fixture

--- db/migrate/049_groups_projects.rb
+++ db/migrate/049_groups_projects.rb
@@ -6,7 +6,7 @@ class GroupsProjects < ActiveRecord::Migration
     end    
     if (ENV['RAILS_ENV'] == 'development')
       directory = File.join(File.dirname(__FILE__), "dev_data")
-      Fixtures.create_fixtures(directory, "groups_projects")
+      Fixtures.create_fixtures(directory, "groups_projects") if File.exists? "#{directory}/groups_projects.yml"
     end
   end
 
