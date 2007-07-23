b1f03ccd32181ee6ef6b4a21a7834f9c7e33c51d
joshmm1 <joshmm1@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-07-23 04:18:19 +0000

    Bus Security - custom activescaffolding -josh

--- app/controllers/bus_admin/bus_secure_actions_controller.rb
+++ app/controllers/bus_admin/bus_secure_actions_controller.rb
@@ -3,7 +3,6 @@ class BusAdmin::BusSecureActionsController < ApplicationController
   active_scaffold :bus_secure_action do |config|
     config.create.columns.exclude :bus_user_types
     config.list.columns.exclude :bus_user_types
-    config.update.columns.exclude :bus_user_types
-   
+    config.update.columns.exclude :bus_user_types   
   end
 end
--- app/controllers/bus_admin/bus_user_types_controller.rb
+++ app/controllers/bus_admin/bus_user_types_controller.rb
@@ -7,29 +7,64 @@ class BusAdmin::BusUserTypesController < ApplicationController
     
   end
   
-  def get_actions
-    controller = BusSecurityLevel.find(params[:bus_security_level_id])
-    @actions=  BusSecureAction.find :all, :conditions => ["bus_security_level_id = ?", controller.id]
-    result = "<select id='record_bus_security_level' name='record[bus_secure_actions][bus_security_level][id]'> "
-    for action in @actions
-      result += "<option value=" + action.id.to_s + ">" + action.permitted_actions + "</option> "
-    end
-    result += "</select>"
-    puts result
-    render :text => result
-  end
+#  def get_actions
+#    controller = BusSecurityLevel.find(params[:bus_security_level_id])
+#    @actions=  BusSecureAction.find :all, :conditions => ["bus_security_level_id = ?", controller.id]
+#    result = "<select id='record_bus_security_level' name='record[bus_secure_actions][bus_security_level][id]'> "
+#    for action in @actions
+#      result += "<option value=" + action.id.to_s + ">" + action.permitted_actions + "</option> "
+#    end
+#    result += "</select>"
+#    puts result
+#    render :text => result
+#  end
   
 #   def get_actions
 #    controller = BusSecurityLevel.find(params[:bus_security_level_id])
 #    @actions=  BusSecureAction.find :all, :conditions => ["bus_security_level_id = ?", controller.id]
-#    #result = "<div id='record_bus_security_level' name='record[bus_secure_actions][bus_security_level][id]'> "
 #    result = "<div style='overflow:auto;width:75px;height:75px;border:1px solid #336699;padding-left:5px'>"
 #    for action in @actions
-#      result += "<input type='checkbox' name=" + record[bus_secure_actions][bus_security_level] + ">" + action.permitted_actions + "</option> "
+#      result += "<input type='checkbox' id='record_bus_security_level' name='record[bus_secure_actions][bus_security_level][id]'> " + action.permitted_actions + "</option> "
 #    end
 #    result += "</div>"
 #    puts result
 #    render :text => result
 #  end
+  
+    def get_actions
+    
+    controller = BusSecurityLevel.find(params[:bus_security_level_id])
+    @actions=  BusSecureAction.find :all, :conditions => ["bus_security_level_id = ?", controller.id]
+    result = "<div style='overflow:auto;width:75px;border:1px solid #336699;'><ul style='padding-left:0; margin-left:0;list-style:none;'>"
+    for action in @actions
+ 
+      result += "<li ><input type='checkbox' id='record_bus_security_level' name='record[bus_secure_actions][" + (action.id - 1).to_s + "][id]' value=" + action.id.to_s + checked(controller,action) +">" + action.permitted_actions + "</input></li>"
+    end  
+    
+    result += "</ui></div>"
+    render :text => result
+  end
+  #checked(controller,action)
+  def checked(controller,action)
+    puts "controller id= " + controller.id.to_s + " action bussecureid = " + action.bus_security_level_id.to_s
+    
+    
+#    @condition BusSecureActionsBusUserType.find :first, :conditions => ["bus_secure_action_id = ?" AND "bus_user_type_id = ? ", action.id, params[:id]]
+#    if @condition != nil
+    bus_user_type = BusUserType.find(session[:bus_user_type_id])
+    checked = "";
+    for bus_secure_action in bus_user_type.bus_secure_actions
+        if bus_secure_action.bus_security_level_id == controller.id && bus_secure_action.id == action.id
+          puts "action: " + bus_secure_action.permitted_actions.to_s + " compared to: " + action.permitted_actions.to_s + " CHECKED"
+          checked =  ' checked="checked"'
+        end
+      end
+    return checked
+  end
+  
+  
+  def prepare_post
+    render :text => "you are tryng to complete"
+  end
   
 end
