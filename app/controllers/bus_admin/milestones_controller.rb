343decdfdc8e0ddcd1198d0d62768ccbc514ee94
pduby <pduby@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-07-23 04:58:50 +0000

    qualify unique milestone name scope add unit tests

--- app/controllers/bus_admin/milestones_controller.rb
+++ app/controllers/bus_admin/milestones_controller.rb
@@ -2,15 +2,15 @@ class BusAdmin::MilestonesController < ApplicationController
   before_filter :login_required
 
   active_scaffold :milestones do |config|
-    config.columns =[ :project, :name, :target_date, :description, :milestone_status, :tasks_count, :tasks ] 
+    config.columns =[ :project, :name, :target_date, :description, :milestone_status, :tasks_count, :tasks ]#:parent_program,
     config.columns[ :name ].label = "Milestone"
     config.columns[ :tasks_count ].label = "Tasks"
-    config.columns[ :project ].form_ui = :select
     config.columns[ :milestone_status ].form_ui = :select
     config.columns[ :milestone_status ].label = "Status"
-    update.columns.exclude [ :project, :tasks_count, :tasks ]
+#    config.columns[ :parent_program ].label = "Program"
+    update.columns.exclude [ :project, :tasks_count, :tasks ]#:parent_program,
     list.columns.exclude [ :description, :tasks ]
-    create.columns.exclude [ :tasks_count ]
+    create.columns.exclude [ :parent_program, :project, :tasks_count ]#:parent_program,
     #show.columns.exclude [ ]
     config.nested.add_link("Tasks", [:tasks])  
   end
