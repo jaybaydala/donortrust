6b687dbc25d11e1e4dfa2dcafe7e6ba49f5f232d
jdoree <jdoree@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-07-26 16:12:38 +0000

    Modifications to bus_user_types to enable editing/saving of actions per controller.

--- app/controllers/bus_admin/bus_user_types_controller.rb
+++ app/controllers/bus_admin/bus_user_types_controller.rb
@@ -3,11 +3,11 @@ class BusAdmin::BusUserTypesController < ApplicationController
   active_scaffold :bus_user_types do |config|
     #config.columns[:bus_secure_actions].ui_type = :select
     #config.create.columns.exclude :bus_secure_actions
-    #config.columns[:bus_secure_levels].association.reverse = :bus_secure_actions
-    
+    #config.columns[:bus_secure_levels].association.reverse = :bus_secure_actions    
   end
     
    def get_actions    
+     
     controller = BusSecurityLevel.find(params[:bus_security_level_id])
     @actions=  BusSecureAction.find :all, :conditions => ["bus_security_level_id = ?", controller.id]
     result = "<div style='overflow:auto;width:75px;border:1px solid #336699;'><ul style='padding-left:0; margin-left:0;list-style:none;'>"
@@ -18,26 +18,7 @@ class BusAdmin::BusUserTypesController < ApplicationController
     result += "</ul></div>"
     render :text => result
   end
-  
-  def checked(controller,action)
     
-#    @condition BusSecureActionsBusUserType.find :first, :conditions => ["bus_secure_action_id = ?" AND "bus_user_type_id = ? ", action.id, params[:id]]
-#    if @condition != nil
-    
-    checked = "";
-    if(params[:bus_user_type_id])
-      bus_user_type = BusUserType.find(params[:bus_user_type_id])
-      for bus_secure_action in bus_user_type.bus_secure_actions
-          if bus_secure_action.bus_security_level_id == controller.id && bus_secure_action.id == action.id
-            puts "action: " + bus_secure_action.permitted_actions.to_s + " compared to: " + action.permitted_actions.to_s + " CHECKED"
-            checked =  ' checked="checked"'
-          end
-        end
-    end
-    return checked
-  end
-  
-  
   def prepare_post
     render :text => "you are tryng to complete"
   end
--- app/helpers/bus_admin/bus_user_types_helper.rb
+++ app/helpers/bus_admin/bus_user_types_helper.rb
@@ -1,11 +1,17 @@
 module BusAdmin::BusUserTypesHelper
 
   
-#  def bus_secure_actions_form_column(record, input_name)
-#        puts "Record = " + record.bus_secure_actions.id.to_s + " input_name = " + input_name
-#        
-#        render :partial => 'bus_admin/bus_account/controller_actions'  
-#       
-#        
-#  end
+  def checked(controller, action, bus_user_type_id)       
+    checked = "";
+    if(bus_user_type_id)
+      bus_user_type = BusUserType.find(bus_user_type_id)
+      for bus_secure_action in bus_user_type.bus_secure_actions
+          if bus_secure_action.bus_security_level_id == controller.id && bus_secure_action.id == action.id
+            puts "action: " + bus_secure_action.permitted_actions.to_s + " compared to: " + action.permitted_actions.to_s + " CHECKED"
+            checked =  ' checked="checked"'
+          end
+        end
+    end
+    return checked
+  end
 end
