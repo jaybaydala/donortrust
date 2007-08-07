da21cb6b828f9294b395e7a0265dcace430fce03
joshmm1 <joshmm1@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-08-07 04:13:30 +0000

    Security Enhancements -josh

--- db/migrate/033_bus_secure_actions_bus_user_types.rb
+++ db/migrate/033_bus_secure_actions_bus_user_types.rb
@@ -1,6 +1,6 @@
 class BusSecureActionsBusUserTypes < ActiveRecord::Migration
   def self.up
-    create_table :bus_secure_actions_bus_user_types, :force => true do |t|
+    create_table :bus_secure_actions_bus_user_types,:id => false, :force => true do |t|
       t.column :bus_secure_action_id,            :int
       t.column :bus_user_type_id,           :int
     end
