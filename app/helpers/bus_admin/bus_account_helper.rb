da0eabf59bed018bb193f7ac5a0497030a208ed8
joshmm1 <joshmm1@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-08-07 04:11:38 +0000

    Security Enhancements -josh

--- app/helpers/bus_admin/bus_account_helper.rb
+++ app/helpers/bus_admin/bus_account_helper.rb
@@ -1,9 +1,5 @@
 module BusAccountHelper
-  def password_form_column(record, field_name)
-    password_field_tag field_name, record.password
-  end
-  def password_confirmation_form_column(record, field_name)
-    password_field_tag field_name, record.password_confirmation
-  end
+
+
 
 end
--- app/helpers/bus_admin/bus_users_helper.rb
+++ app/helpers/bus_admin/bus_users_helper.rb
@@ -1,2 +1,4 @@
 module BusAdmin::BusUsersHelper
+
+
 end
