b5557cd4d34fb35aba3033edbfe079075e27a497
pduby <pduby@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-08-12 03:52:12 +0000

    bug 12977 remove milestones count from milestone_status CRUD screens

--- app/controllers/bus_admin/milestone_statuses_controller.rb
+++ app/controllers/bus_admin/milestone_statuses_controller.rb
@@ -4,22 +4,10 @@ class BusAdmin::MilestoneStatusesController < ApplicationController
   include ApplicationHelper
 
   active_scaffold :milestone_statuses do |config|
-    config.columns =[ :name, :description, :milestones_count, :milestones ]
-    #config.columns[ :name ].label = "Title"
-    config.columns[ :milestones_count ].label = "Milestones"
-    list.columns.exclude [ :milestones ]
-    update.columns.exclude [ :milestones_count, :milestones ]
-    create.columns.exclude [ :milestones_count, :milestones ]
-    #show.columns.exclude [ ]
+    config.columns =[ :name, :description ]
+    #list.columns.exclude [  ]
+    #update.columns.exclude [  ]
+    #create.columns.exclude [  ]
+    #show.columns.exclude [  ]
   end
-
-#  def destroy
-#    begin
-#      super.destroy
-#    rescue
-#      @error = "You cannot delete this status; it is being used by a Milestone."
-#      flash[:error] = @error #for some reason this won't display
-#      show_message_and_reset(@error, "error")
-#    end
-#  end
 end
--- app/models/milestone_status.rb
+++ app/models/milestone_status.rb
@@ -1,9 +1,8 @@
 class MilestoneStatus < ActiveRecord::Base
   has_many :milestones
 
-  validates_presence_of :name
+  validates_presence_of :name, :description
   validates_uniqueness_of :name
-  validates_presence_of :description
 
   def destroy
     result = false
@@ -14,9 +13,5 @@ class MilestoneStatus < ActiveRecord::Base
       result = super
     end
     return result
-  end
-
-  def milestones_count
-    return milestones.count
   end
 end
