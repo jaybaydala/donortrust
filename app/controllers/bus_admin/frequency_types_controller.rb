c4c0e7789f330397ddd5822261cad0b411ec3910
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-09-27 22:45:38 +0000

    removed active field (caused errors)

--- app/controllers/bus_admin/frequency_types_controller.rb
+++ app/controllers/bus_admin/frequency_types_controller.rb
@@ -2,7 +2,7 @@ class BusAdmin::FrequencyTypesController < ApplicationController
  before_filter :login_required, :check_authorization
 
   active_scaffold :frequency_type do |config|
-    config.columns =[ :name, :active, :indicator_measurement_count ]
+    config.columns =[ :name, :indicator_measurement_count ]
     config.columns[ :indicator_measurement_count ].label = "Reference Count"
     list.columns.exclude [ :indicator_measurement_count ]
     update.columns.exclude [ :indicator_measurement_count ]
