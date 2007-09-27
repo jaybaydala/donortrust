4ef3b078b3d107134d3e9112b66bddf78eb25a33
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-09-27 22:52:26 +0000

    quick facts still need you tube and flickr

--- app/controllers/bus_admin/quick_fact_places_controller.rb
+++ app/controllers/bus_admin/quick_fact_places_controller.rb
@@ -1,12 +1,13 @@
 class BusAdmin::QuickFactPlacesController < ApplicationController
-
-   active_scaffold :quick_fact_places do |config|
+  before_filter :login_required#, :check_authorization
+  
+  active_scaffold :quick_fact_places do |config|
     config.label = "Quick Facts"
-    config.columns = [ :quick_fact,:description, :place]    
+    config.columns = [ :quick_fact,:description, :place ]    
     config.columns[ :quick_fact ].form_ui = :select
     config.columns[ :place ].form_ui = :select    
-    update.columns.exclude [:place]
-
+    update.columns.exclude [:place]    
+ #   config.action_links.add 'index', :label => '<img src="/images/icons/you_tube.png" border=0>', :page => true, :type=> :record, :parameters =>{:controller=>"bus_admin/quick_fact_place_you_tube_videos"}
+    config.action_links.add 'index', :label => '<img src="/images/icons/you_tube.png" border=0>', :page => true, :type=> :record, :parameters =>{:controller=>"bus_admin/project_you_tube_videos"}
   end
-
 end
