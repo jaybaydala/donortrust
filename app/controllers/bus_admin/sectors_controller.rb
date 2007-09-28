eab3ae60444ac37b273b998d99982edda4e0e15d
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-09-28 18:05:50 +0000

    Removed country, region, city… Added place

--- app/controllers/bus_admin/sectors_controller.rb
+++ app/controllers/bus_admin/sectors_controller.rb
@@ -1,14 +1,14 @@
 class BusAdmin::SectorsController < ApplicationController
-  before_filter :login_required, :check_authorization
+  before_filter :login_required#, :check_authorization
 
   active_scaffold :sectors do |config|
-    config.columns =[ :name, :description, :project_count, :country_count, :country_sectors ]
+    config.columns =[ :name, :description, :project_count, :country_count, :place_sectors ]
     config.columns[ :name ].label = "Sector"
-    config.nested.add_link("Countries", [:country_sectors])
-    list.columns.exclude [ :project_count, :country_count, :country_sectors ]
-    update.columns.exclude [ :project_count, :country_count, :country_sectors ]
-    create.columns.exclude [ :project_count, :country_count, :country_sectors ]
-    show.columns.exclude [ :country_sectors ]
+    config.nested.add_link("Countries", [:place_sectors])
+    list.columns.exclude [ :project_count, :country_count, :place_sectors ]
+    update.columns.exclude [ :project_count, :country_count, :place_sectors ]
+    create.columns.exclude [ :project_count, :country_count, :place_sectors ]
+    show.columns.exclude [ :place_sectors ]
     config.action_links.add 'inactive_records', :label => 'Show Inactive', :parameters =>{:action => 'inactive_records'}
     config.nested.add_link("Quick Fact", [:quick_fact_sectors])
     config.nested.add_link("Sub-Category", [:causes])
