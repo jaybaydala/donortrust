2bd356e9a5c4e9e0d3472036148be12f57d2559d
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-01 22:02:51 +0000

    Added image upload, blog_url, rss_feed

--- app/controllers/bus_admin/places_controller.rb
+++ app/controllers/bus_admin/places_controller.rb
@@ -2,14 +2,20 @@ class BusAdmin::PlacesController < ApplicationController
   before_filter :login_required#, :check_authorization
   
   active_scaffold :places do |config|
-    config.columns =[ :name, :description, :place_type ]
+    config.columns =[ :name, :description, :place_type, :file,:blog_url, :rss_feed  ]
+    list.columns.exclude [ :blog_url, :rss_feed ]
     config.columns[ :place_type ].form_ui = :select
+    config.columns[ :rss_feed ].form_ui = :select
     config.nested.add_link("Next", [:children]) 
     config.nested.add_link("Quick Fact", [:quick_fact_places]) 
+    config.columns[ :file ].label = "Image File"
+    config.create.multipart = true
+    config.update.multipart = true
+  
   end
   
   def list
     @places= Place.find_all_by_parent_id(nil)
   end
-  
+   
 end
