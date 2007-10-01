02b678b547592e1be9bb6d4b842baa500570d0cc
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-01 16:17:05 +0000

    Removed cause added acts_as_tree

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,13 +1,13 @@
 class Sector < ActiveRecord::Base
   acts_as_paranoid
+  acts_as_tree :order=>"name"
   
   has_and_belongs_to_many :projects
   has_many :place_sectors
   
   has_many :places , :through => :place_sectors
   has_many :quick_fact_sectors   
-  has_many :causes 
-   
+     
   validates_presence_of :name, :description
   validates_uniqueness_of :name
 
@@ -15,7 +15,4 @@ class Sector < ActiveRecord::Base
     return projects.count
   end
 
-#  def country_count
-#    return countries.count
-#  end
 end
