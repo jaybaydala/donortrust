d1979d4faec47a7dbf0a5a8dbafb40a2ad3fc9dc
pduby <pduby@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-02 03:27:18 +0000

    delete unused partner_version model since now handled automatically by acts_as_versioned

--- app/helpers/bus_admin/partners_helper.rb
+++ app/helpers/bus_admin/partners_helper.rb
@@ -1,16 +1,7 @@
 module BusAdmin::PartnersHelper
-#  def partner_histories_column(record)
-#    link_to "Show history", {:controller => 'partner_histories', :action => 'list', :partner_id => record.id}
-#  end
-  
-  def partner_versions_column(record)
-    link_to "History", {:controller => 'bus_admin/partner_versions', :action => 'list', :partner_id => record.id }
-  end
-  
   def note_column(record)
-     if record.note?
+    if record.note?
       link_to_remote_redbox image_tag('note2.png'), :url => {:controller => 'bus_admin/partners', :action => 'show_note', :id => record.id}
-     end  
-   end
-   
+    end  
+  end
 end
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,12 +2,8 @@ require 'acts_as_paranoid_versioned'
 class Partner < ActiveRecord::Base
   acts_as_paranoid_versioned
 
-#  after_save  :create_partner_history
-#  after_save :save_partner_history
-
   belongs_to    :partner_type
   belongs_to    :partner_status
-  has_many      :partner_versions
   has_many      :projects
   has_and_belongs_to_many :contacts #is this the right relationship? 
   has_many      :programs, :through => :projects
@@ -28,23 +24,6 @@ class Partner < ActiveRecord::Base
     end
   end
 
-  def create_partner_history
-    if Partner.exists?(self.id)
-      @create_partner_history_ph = PartnerHistory.new_audit(Partner.find(self.id))
-    end
-  end
-  
-  def save_partner_history
-    if (@create_partner_history_ph)
-      @create_partner_history_ph.save
-      @create_partner_history_ph = nil
-    end
-  end
-  
-  def self.is_a_partner?(object)
-    return object.class.to_s == "Partner"
-  end
-  
   def total_projects    
     return (self.projects.find(:all)).size
   end
