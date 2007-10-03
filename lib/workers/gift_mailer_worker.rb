71514c88d030b595c042e723f1cfd4cb4534613e
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-03 19:42:32 +0000

    Adding backgroundrb plugin and slave and daemons gems Adding gift_mailer script to work with backgroundrb

---
+++ lib/workers/gift_mailer_worker.rb
@@ -0,0 +1,24 @@
+# Put your code that runs your task inside the do_work method it will be
+# run automatically in a thread. You have access to all of your rails
+# models.  You also get logger and results method inside of this class
+# by default.
+class GiftMailerWorker < BackgrounDRb::Worker::RailsBase
+  
+  def do_work(args)
+    # This method is called in it's own new thread when you
+    # call new worker. args is set to :args
+    num_sent = 0
+    logger.info "[#{Time.now.to_s}] Checking for scheduled Gifts to Email"
+    gifts = find_records
+    gifts.each do |g|
+      num_sent+=1
+      g.send_gift_mail
+    end
+    logger.info "[#{Time.now.to_s}] Scheduled Gift Emails Sent: #{num_sent}"
+  end
+
+  def find_records(send_at_time = Time.now)
+    Gift.find(:all, :conditions=>['sent_at is null AND send_at <= ?', send_at_time.to_s(:db)])
+  end
+end
+GiftMailerWorker.register
