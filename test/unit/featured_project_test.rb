d83cb6a277037cbe062f01e849083232581a876a
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-04 21:18:35 +0000

    Removing featured_project model, tests and yml files

--- test/unit/featured_project_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-
-class FeaturedProjectTest < Test::Unit::TestCase
-  fixtures :featured_projects
-
-  def test_truth
-    assert true
-  end
-end
