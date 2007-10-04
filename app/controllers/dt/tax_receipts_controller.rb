001f22b9011a6d08d3cfe412d900f4d5d7d50174
plucas <plucas@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-04 13:27:55 +0000

    latest tax receipt dupe template formatted

--- app/controllers/dt/tax_receipts_controller.rb
+++ app/controllers/dt/tax_receipts_controller.rb
@@ -1,26 +1,31 @@
 require 'pdf/writer'
+require 'action_view/helpers/number_helper'
+include ActionView::Helpers::NumberHelper
+
 class Dt::TaxReceiptsController < DtApplicationController
   before_filter :login_required, :only => [ :printable ]
 
-  # say something nice, you goof!  something sweet.
   def printable
     @receipt = TaxReceipt.find(id=params[:id])
     _pdf = PDF::Writer.new
     _pdf.select_font "Times-Roman"
     _pdf.compressed=true
-    i0 = _pdf.image File.dirname(__FILE__) + "/cf_tax_receipt.jpg"
+    i0 = _pdf.image File.dirname(__FILE__) + "/tax_receipt-duplicate.png"
     # TODO: re-align once final receipt template is received
-    x = 100
-    _pdf.add_text(x, 151, @receipt[:first_name] + ' ' + @receipt[:last_name], 12)
-    _pdf.add_text(x, 141, @receipt.investment.amount, 12)
-    _pdf.add_text(x, 131, @receipt.created_at.to_s(:short), 12)
-    _pdf.add_text(x, 121, @receipt.investment.created_at.to_s(:short), 12)
-    _pdf.add_text(x, 111, @receipt.address, 12)
-    _pdf.add_text(x, 101, @receipt.city, 12)
-    _pdf.add_text(x, 91, @receipt.province, 12)
-    _pdf.add_text(x, 81, @receipt.postal_code, 12)
-    _pdf.add_text(x, 71, @receipt.country, 12)
-    #_pdf.encrypt(user_pass='something', owner_pass='something else', permissions=[:print])
+    x = 227
+    font_size = 8
+    _pdf.add_text(x+14, 639, @receipt.id_display, font_size)
+    _pdf.add_text(x, 625, number_to_currency(@receipt.investment.amount), font_size)
+    _pdf.add_text(x, 612, @receipt.created_at.to_s(), font_size)
+    _pdf.add_text(x, 598, @receipt.investment.created_at.to_s(), font_size)
+    x2 = 187
+    _pdf.add_text(x2, 565, @receipt[:first_name] + ' ' + @receipt[:last_name], font_size)
+    _pdf.add_text(x2, 549, @receipt.address, font_size)
+    _pdf.add_text(x2, 533, @receipt.city, font_size)
+    x3 = 367
+    _pdf.add_text(x3, 533, @receipt.province, font_size)
+    _pdf.add_text(x2, 517, @receipt.postal_code, font_size)
+    _pdf.add_text(x3, 517, @receipt.country, font_size)
     send_data _pdf.render, :filename => "CFTaxReceipt-#{@receipt.id_display}.pdf", :type => "application/pdf"
   end
 
