a60c48e1042554f202337a49567852ad2a6808cd
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-05 17:37:29 +0000

    added description to list.columns.exclude

--- app/controllers/bus_admin/places_controller.rb
+++ app/controllers/bus_admin/places_controller.rb
@@ -2,8 +2,8 @@ class BusAdmin::PlacesController < ApplicationController
   before_filter :login_required#, :check_authorization
   
   active_scaffold :places do |config|
-    config.columns =[ :name, :description, :place_type, :file,:blog_url, :rss_feed  ]
-    list.columns.exclude [ :blog_url, :rss_feed ]
+    config.columns =[ :name, :place_type, :file,:blog_url, :rss_feed , :description ]
+    list.columns.exclude [ :blog_url, :rss_feed, :description ]
     config.columns[ :place_type ].form_ui = :select
     config.columns[ :rss_feed ].form_ui = :select
     config.nested.add_link("Next", [:children]) 
@@ -17,5 +17,5 @@ class BusAdmin::PlacesController < ApplicationController
   def list
     @places= Place.find_all_by_parent_id(nil)
   end
-   
-end
+end 
+  
