09fb484b5aa2d819796be7a0890368c2a7e50409
tadatoshi <tadatoshi@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-09 04:23:05 +0000

    Modified to generate tests in the similar way as ActiveScaffold generates codes.

--- test/functional/bus_admin/budget_items_controller_test.rb
+++ test/functional/bus_admin/budget_items_controller_test.rb
@@ -11,47 +11,12 @@ class BusAdmin::BudgetItemsControllerTest < Test::Unit::TestCase
     @controller = BusAdmin::BudgetItemsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    @budget_item = budget_items(:budget_item_one)
   end
 
-  def test_should_get_index
-    get :index
-    assert_response :success
-    assert assigns(:budget_items)
+  should_be_restful do |resource|
+    resource.klass = BudgetItem
+    resource.formats = [:html]
   end
 
-  def test_should_get_new
-    get :new
-    assert_response :success
-  end
-  
-  def test_should_create_budget_item
-    old_count = budget_item.count
-    post :create, :budget_item => { }
-    assert_equal old_count+1, budget_item.count
-    
-    assert_redirected_to budget_item_path(assigns(:budget_item))
-  end
-
-  def test_should_show_budget_item
-    get :show, :id => 1
-    assert_response :success
-  end
-
-  def test_should_get_edit
-    get :edit, :id => 1
-    assert_response :success
-  end
-  
-  def test_should_update_budget_item
-    put :update, :id => 1, :budget_item => { }
-    assert_redirected_to budget_item_path(assigns(:budget_item))
-  end
-  
-  def test_should_destroy_budget_item
-    old_count = budget_item.count
-    delete :destroy, :id => 1
-    assert_equal old_count-1, budget_item.count
-    
-    assert_redirected_to bus_admin_budget_items_path
-  end
 end
