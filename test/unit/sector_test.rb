8fafec65c78ab2778c736f7ba2a68ca2df4079f7
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-09 15:45:17 +0000

    sector_test updates

--- test/unit/sector_test.rb
+++ test/unit/sector_test.rb
@@ -4,27 +4,20 @@ require File.dirname(__FILE__) + '/../test_helper'
 context "Sectors" do
   fixtures :sectors
 
-setup do
+  setup do
     @sector = Sector.find(1)
   end
  
-  specify "The sector should have a name & description" do
-    @sector.name.should.not.be.nil
-    @sector.description.should.not.be.nil
+  specify "duplicate name should not validate" do
+    @sector1 = Sector.new( :name => @sector.name, :description =>  "This should be invalid" )
+    @sector1.should.not.validate
   end
 
-
-specify "duplicate name should not validate" do
-    @sector1 = Sector.new( :name => @sector.name, :description =>  "This should be invalid" )
-    
-    @sector1.should.not.validate
-    
-end
-specify "nil name should not validate" do
+  specify "nil name should not validate" do
     @sector.name = nil
     @sector.should.not.validate
   end
-  
+
   specify "nil description should not validate" do
     @sector.description = nil
     @sector.should.not.validate
