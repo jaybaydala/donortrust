2223c171023a5f721a5dd451f1302762d382a1cd
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-17 23:00:44 +0000

    Changed default upload folder

--- app/models/e_card.rb
+++ app/models/e_card.rb
@@ -1,5 +1,5 @@
 class ECard < ActiveRecord::Base
-  file_column :file
+  file_column :file, :web_root => "images/bus_admin/uploads/", :root_path => File.join(RAILS_ROOT, "public/images/bus_admin/uploads")
   
   validates_presence_of :name
   
