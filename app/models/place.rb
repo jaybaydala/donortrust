e79fb1b448b0f366443dd95de3b3221ba358e676
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-17 23:00:25 +0000

    Changed default upload folder

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
   acts_as_tree :order=>"name"
-  file_column :file
+  file_column :file, :web_root => "images/bus_admin/uploads/", :root_path => File.join(RAILS_ROOT, "public/images/bus_admin/uploads")
   
   belongs_to :place_type
   belongs_to :sector
