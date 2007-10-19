3e00ec673d814294226ab8d5e1cd7fbc5f9bf39f
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-19 20:35:14 +0000

    Added textile editor

--- app/helpers/bus_admin/partners_helper.rb
+++ app/helpers/bus_admin/partners_helper.rb
@@ -3,5 +3,23 @@ module BusAdmin::PartnersHelper
     if record.note?
       link_to_remote_redbox image_tag('/images/bus_admin/note2.png'), :url => {:controller => 'bus_admin/partners', :action => 'show_note', :id => record.id}
     end  
-  end
+  end        
+  def description_column(record)
+    SuperRedCloth.new(record.description).to_html
+  end        
+  def business_model_column(record)
+    SuperRedCloth.new(record.business_model).to_html
+  end        
+  def funding_sources_column(record)
+    SuperRedCloth.new(record.funding_sources).to_html
+  end  
+  def mission_statement_column(record)
+    SuperRedCloth.new(record.mission_statement).to_html
+  end        
+  def philosophy_dev_column(record)
+    SuperRedCloth.new(record.philosophy_dev).to_html
+  end        
+#  def note_column(record)
+#    SuperRedCloth.new(record.note).to_html
+#  end
 end
