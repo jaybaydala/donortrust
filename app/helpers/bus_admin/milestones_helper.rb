af476641bb4fbd4e8b6f699ff4aabbe8c86bc33b
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-19 22:03:00 +0000

    Added textile editor

--- app/helpers/bus_admin/milestones_helper.rb
+++ app/helpers/bus_admin/milestones_helper.rb
@@ -1,7 +1,7 @@
 module BusAdmin::MilestonesHelper
   def description_column(record)
-    if record.description!= nil 
-      SuperRedCloth.new(record.description).to_html
+    if record.description != nil 
+      RedCloth.new(record.description).to_html
     end
   end
 end
