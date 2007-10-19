4353f2bb1de1294c1b8d32bb5c91271a1e68e66a
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-19 21:16:18 +0000

    Added textile editor

--- app/helpers/bus_admin/groups_helper.rb
+++ app/helpers/bus_admin/groups_helper.rb
@@ -6,5 +6,11 @@ module BusAdmin::GroupsHelper
       result = "No"   
     end
   return result
-  end
+end
+
+  def description_column(record)
+    if record.description!= nil 
+      SuperRedCloth.new(record.description).to_html
+    end
+  end  
 end
