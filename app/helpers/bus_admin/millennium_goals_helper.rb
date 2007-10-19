e9795b36cea9c97142bee0e860601af782d0ed1c
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-19 21:16:30 +0000

    Added textile editor

--- app/helpers/bus_admin/millennium_goals_helper.rb
+++ app/helpers/bus_admin/millennium_goals_helper.rb
@@ -1,2 +1,7 @@
 module BusAdmin::MillenniumGoalsHelper
+  def description_column(record)
+    if record.description!= nil 
+      SuperRedCloth.new(record.description).to_html
+    end
+  end  
 end
