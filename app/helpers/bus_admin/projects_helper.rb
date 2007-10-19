b1fa8b50180757bef2098267fe42e32ce04d3994
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-19 21:33:24 +0000

    Added textile editor

--- app/helpers/bus_admin/projects_helper.rb
+++ app/helpers/bus_admin/projects_helper.rb
@@ -17,4 +17,41 @@ module BusAdmin::ProjectsHelper
       link_to_remote_redbox image_tag('/images/bus_admin/note2.png'), :url => {:controller => 'bus_admin/projects', :action => 'show_project_note', :id => record.id}
      end  
    end
+  
+  def description_column(record)
+     if record.description != nil 
+       SuperRedCloth.new(record.description).to_html
+    end    
+  end
+  
+  def intended_outcome_column(record)
+     if record.intended_outcome != nil 
+        SuperRedCloth.new(record.intended_outcome).to_html
+    end   
+  end
+  
+  def meas_eval_plan_column(record)
+     if record.meas_eval_plan != nil 
+       SuperRedCloth.new(record.meas_eval_plan).to_html
+    end    
+  end
+  
+  def project_in_community_column(record)
+     if record.project_in_community != nil 
+       SuperRedCloth.new(record.project_in_community).to_html
+    end    
+  end
+  
+  def other_projects_column(record)
+     if record.other_projects != nil 
+        SuperRedCloth.new(record.other_projects).to_html
+    end   
+  end
+  
+#  def   responsibilities_column(record)
+#     if record.responsibilities != nil 
+#    end
+#    SuperRedCloth.new(record.responsibilities).to_html
+#  end
+    
 end
