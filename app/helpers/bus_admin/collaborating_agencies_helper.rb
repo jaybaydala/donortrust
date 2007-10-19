738876118f5ffd7c17fb00ea04ffcc483f82212c
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-19 21:21:53 +0000

    Added textile editor

--- app/helpers/bus_admin/collaborating_agencies_helper.rb
+++ app/helpers/bus_admin/collaborating_agencies_helper.rb
@@ -1,2 +1,7 @@
 module BusAdmin::CollaboratingAgenciesHelper
+  def responsibilities_column(record)
+    if record.responsibilities!= nil 
+      SuperRedCloth.new(record.responsibilities).to_html
+    end
+  end
 end
