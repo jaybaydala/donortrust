d4204a2c77a128c4b927e8b2e1b12abdc49f7bc0
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-23 18:38:39 +0000

    More backgroundrb schedule fixes. Don't use done_working! (old version), just use exit, a Kernel call which exits the current thread

--- lib/workers/gift_mailer_worker.rb
+++ lib/workers/gift_mailer_worker.rb
@@ -8,17 +8,17 @@ class GiftMailerWorker < BackgrounDRb::Worker::RailsBase
     # This method is called in it's own new thread when you
     # call new worker. args is set to :args
     num_sent = 0
-    logger.info "[#{Time.now.to_s}] Checking for scheduled Gifts to Email"
+    logger.info "[#{Time.now.utc.to_s}] Checking for scheduled Gifts to Email"
     gifts = find_records
     gifts.each do |g|
       num_sent+=1
       g.send_gift_mail
     end
-    logger.info "[#{Time.now.to_s}] Scheduled Gift Emails Sent: #{num_sent}"
-    done_working! # This is required when the job is done!
+    logger.info "[#{Time.now.utc.to_s}] Scheduled Gift Emails Sent: #{num_sent}"
+    exit # This is required when the job is done!
   end
 
-  def find_records(send_at_time = Time.now)
+  def find_records(send_at_time = Time.now.utc)
     Gift.find(:all, :conditions=>['sent_at is null AND send_at <= ?', send_at_time.to_s(:db)])
   end
 end
--- lib/workers/session_cleaner_worker.rb
+++ lib/workers/session_cleaner_worker.rb
@@ -3,8 +3,8 @@ class SessionCleanerWorker < BackgrounDRb::Worker::RailsBase
     # This method is called in it's own new thread when you
     # call new worker. args is set to :args
     CGI::Session::ActiveRecordStore::Session.destroy_all( ['updated_at <?', 20.minutes.ago] ) 
-    logger.info "[#{Time.now.to_s}] Deleted inactive sessions"
-    done_working! # This is required when the job is done!
+    logger.info "[#{Time.now.utc.to_s}] Deleted inactive sessions"
+    exit # This is required when the job is done!
   end
 end
 SessionCleanerWorker.register
