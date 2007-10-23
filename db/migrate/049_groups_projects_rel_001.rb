9a113082f147397233ec9e9d4c85a5ffd4e06555
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-23 13:48:31 +0000

    Added indexes to tables that the front-end is concerned with. This should help to keep the speed up as we grow records

--- db/migrate/049_groups_projects_rel_001.rb
+++ db/migrate/049_groups_projects_rel_001.rb
@@ -3,7 +3,8 @@ class GroupsProjectsRel001 < ActiveRecord::Migration
     create_table :groups_projects do |t|
       t.column :group_id,         :int
       t.column :project_id,       :int
-    end    
+    end
+    
     if (ENV['RAILS_ENV'] == 'development')
       directory = File.join(File.dirname(__FILE__), "dev_data")
       Fixtures.create_fixtures(directory, "groups_projects") if File.exists? "#{directory}/groups_projects.yml"
