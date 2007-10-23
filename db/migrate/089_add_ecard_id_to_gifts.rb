aa0e670500e17d7fe2a56dcc87a9359c305f62b0
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-23 17:20:10 +0000

    ecard_id renamed to e_card_id

--- db/migrate/089_add_ecard_id_to_gifts.rb
+++ db/migrate/089_add_ecard_id_to_gifts.rb
@@ -1,13 +1,13 @@
 class AddEcardIdToGifts < ActiveRecord::Migration
   def self.up
     remove_column :gifts, :ecard
-    add_column :gifts, :ecard_id, :integer
-    add_index :gifts, :ecard_id
+    add_column :gifts, :e_card_id, :integer
+    add_index :gifts, :e_card_id
   end
 
   def self.down
-    remove_index :gifts, :ecard_id
-    remove_column :gifts, :ecard_id
+    remove_index :gifts, :e_card_id
+    remove_column :gifts, :e_card_id
     add_column :gifts, :ecard, :integer
   end
 end
