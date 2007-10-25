30506527a05fd869cf0870f517b67b0921241e1e
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-25 15:19:32 +0000

    added auto-load dev_data to e_cards

--- db/migrate/075_e_cards_rel_001.rb
+++ db/migrate/075_e_cards_rel_001.rb
@@ -8,6 +8,10 @@ class ECardsRel001 < ActiveRecord::Migration
       t.column :large,     :string     
       t.column :printable, :string     
     end
+    if (ENV['RAILS_ENV'] == 'development')
+      directory = File.join(File.dirname(__FILE__), "dev_data")
+      Fixtures.create_fixtures(directory, "memberships") if File.exists? "#{directory}/e_cards.yml"
+    end
   end
   def self.down
     drop_table :e_cards
