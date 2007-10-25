06478cb91acb82b75c49a3c93a2464fd8f86eec3
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-25 20:17:55 +0000

    Added wishlist boolean to shares table

--- db/migrate/096_add_wishlist_column_to_shares.rb
+++ db/migrate/096_add_wishlist_column_to_shares.rb
@@ -1,7 +1,9 @@
 class AddWishlistColumnToShares < ActiveRecord::Migration
   def self.up
+    add_column :shares, :wishlist, :boolean
   end
 
   def self.down
+    remove_column :shares, :wishlist
   end
 end
