9b02fae8a30d2aa6d2794ec979bd1d5bf161ae91
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-26 01:41:31 +0000

    Fixed migration 097. I don't know if my fix is what was wanted, but I needed it to run.

--- db/migrate/097_project_you_tube_change_you_tube_id.rb
+++ db/migrate/097_project_you_tube_change_you_tube_id.rb
@@ -1,10 +1,10 @@
-class ProjectFlickrChangeFlickrId < ActiveRecord::Migration
+class ProjectYouTubeChangeYouTubeId < ActiveRecord::Migration
   def self.up
-    rename_column(:project_you_tube, :flickr_image_id, :photo_id)
+    rename_column(:project_you_tube_videos, :you_tube_video_id, :you_tube_id)
   end
 
   def self.down
-    rename_column(:project_you_tube, :photo_id, :flickr_image_id)
+    rename_column(:project_you_tube_videos, :you_tube_id, :you_tube_video_id)
   end
 end
 
