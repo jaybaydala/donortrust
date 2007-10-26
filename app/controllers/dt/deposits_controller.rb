8a2062abeaa55aac030e5f1c2163d6dd7b930ebb
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-26 22:18:13 +0000

    Added tax receipt

--- app/controllers/dt/deposits_controller.rb
+++ app/controllers/dt/deposits_controller.rb
@@ -1,4 +1,6 @@
 require 'iats/iats_process.rb'
+require 'pdf_proxy'
+include PDFProxy
 class Dt::DepositsController < DtApplicationController
   include IatsProcess
   before_filter :login_required
@@ -15,6 +17,9 @@ class Dt::DepositsController < DtApplicationController
     
     respond_to do |format|
       if @deposit.authorization_result != nil && @saved = @deposit.save
+        if @deposit.country == 'CA'
+          gift_tax_receipt         
+        end
         flash[:notice] = "Your deposit was successful."
         format.html { redirect_to :controller => '/dt/accounts', :action => 'show', :id => current_user.id }
       else
@@ -23,7 +28,7 @@ class Dt::DepositsController < DtApplicationController
       end
     end
   end
-  
+   
   def confirm
     @deposit = Deposit.new( deposit_params )
     respond_to do |format|
@@ -45,4 +50,24 @@ class Dt::DepositsController < DtApplicationController
     deposit_params[:user_id] = current_user.id
     deposit_params
   end
+  
+  def gift_tax_receipt
+
+    @deposit_tax_receipt = TaxReceipt.new( params[:tax_receipt] ) 
+    if logged_in?
+      @deposit_tax_receipt.user = current_user
+    end
+    @deposit_tax_receipt.first_name = @deposit.first_name
+    @deposit_tax_receipt.last_name = @deposit.last_name
+    @deposit_tax_receipt.address = @deposit.address
+    @deposit_tax_receipt.city = @deposit.city    
+    @deposit_tax_receipt.province = @deposit.province
+    @deposit_tax_receipt.postal_code = @deposit.postal_code
+    @deposit_tax_receipt.country = @deposit.country    
+    @deposit_tax_receipt.email = current_user[:login]
+    @deposit_tax_receipt.deposit_id = @deposit.id    
+    @deposit_tax_receipt.save
+    @deposit_tax_receipt.send_tax_receipt     
+  end
+
 end
