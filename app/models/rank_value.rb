584d952ce27cda845af97efdadb243105f436387
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-10-29 16:14:33 +0000

    Fixed data validation

--- app/models/rank_value.rb
+++ app/models/rank_value.rb
@@ -1,22 +1,22 @@
 class RankValue < ActiveRecord::Base
    has_many :ranks
    
-   file_column :file, :web_root => "images/bus_admin/uploads/", :root_path => File.join(RAILS_ROOT, "public/images/bus_admin/uploads")
-   validates_presence_of :file
-    
- validates_presence_of :rank_value, :if => :check_validation?
-  validates_numericality_of :rank_value
+  file_column :file, :web_root => "images/bus_admin/uploads/", :root_path => File.join(RAILS_ROOT, "public/images/bus_admin/uploads")
+  validates_presence_of :file
   
+  validates_numericality_of :rank_value
+  validates_presence_of :rank_value, :if => :check_validation?
+    
   protected 
   def check_validation?
-    if rank != nil
-      rank <= 4 
+    if rank_value != nil
+      rank_value <= 4 
     end
   end
     
   def validate
-    if rank != nil
-      errors.add(:rank_value, "Rank must be between 0 and 4" ) if rank > 4 or rank < 0 #or rank = nil
+    if rank_value != nil
+      errors.add(:rank_value, "Rank must be between 0 and 4" ) if rank_value > 4 or rank_value < 0 #or rank = nil
     end
   end  
 end
