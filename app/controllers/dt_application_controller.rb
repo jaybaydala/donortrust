b0449625278fa16129f066a134a899917eeff4f3
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-11-01 19:11:49 +0000

    removed 'development' from ssl_filter

--- app/controllers/dt_application_controller.rb
+++ app/controllers/dt_application_controller.rb
@@ -28,7 +28,7 @@ class DtApplicationController < ActionController::Base
 
   protected
   def ssl_filter
-    if ['staging', 'production', 'development'].include?(ENV['RAILS_ENV'])
+    if ['staging', 'production'].include?(ENV['RAILS_ENV'])
       redirect_to url_for(:protocol => 'https://') and return false if !request.ssl? && ssl_required? 
       redirect_to url_for(:protocol => 'http://') and return false if request.ssl? && !ssl_required? 
     end
