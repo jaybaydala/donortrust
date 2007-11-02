bed6c06336e8d3e5f2291e3e234439f2d395556b
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-11-02 14:59:13 +0000

    Added facebook id

--- app/controllers/bus_admin/places_controller.rb
+++ app/controllers/bus_admin/places_controller.rb
@@ -2,8 +2,8 @@ class BusAdmin::PlacesController < ApplicationController
  #before_filter :login_required, :check_authorization
   
   active_scaffold :places do |config|
-    config.columns =[ :name, :place_type, :file,:blog_url, :rss_url , :description ]
-    list.columns.exclude [ :blog_url, :rss_url, :description ]
+    config.columns =[ :name, :place_type, :file,:blog_url, :rss_url , :facebook_group_id, :description ]
+    list.columns.exclude [ :blog_url, :rss_url, :description, :facebook_group_id ]
     config.columns[ :place_type ].form_ui = :select
     
    config.columns[:children].association.reverse = :parent
@@ -12,6 +12,7 @@ class BusAdmin::PlacesController < ApplicationController
     config.nested.add_link("Quick Fact", [:quick_fact_places]) 
     config.nested.add_link("Sectors", [:place_sectors]) 
     config.columns[ :file ].label = "Image File"
+    config.columns[ :facebook_group_id ].label = "Facebook Group Id"
     config.create.multipart = true
     config.update.multipart = true
     
