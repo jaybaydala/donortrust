994f59ca1be8a12e0f8f406c678ce269adc23f64
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-11-04 21:24:55 +0000

    Consolidated project template to more closely resemble each other. Limited "Projects in this community" to other projects (doesn't include the current one)

--- app/helpers/dt/projects_helper.rb
+++ app/helpers/dt/projects_helper.rb
@@ -18,4 +18,17 @@ module Dt::ProjectsHelper
   def current_member(group, user = current_user)
     @current_member ||= group.memberships.find_by_user_id(user) if user
   end
+  
+  def community_projects(project)
+    if @community_projects.nil?
+      @community_projects = []
+      unless @project.community.nil? || @project.community.projects.nil?
+        projects = Project.find_public(:all, :select => 'projects.name, projects.id', :conditions => ['projects.place_id = ? AND projects.id != ?', project.community_id, project.id])
+        @community_projects = projects.collect do |project|
+          [project.name, project.id]
+        end
+      end
+    end
+    @community_projects
+  end
 end
