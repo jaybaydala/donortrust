5b07385884edf91ac6f11e4b67a9465a3a9405dd
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2007-11-08 21:49:18 +0000

    Prepping for 1.0.2 release

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require 'mongrel_cluster/recipes'
 set :application, "donortrust"
-set :repository,  "http://#{application}.rubyforge.org/svn/tags/rel_1-0-1/"
+set :repository,  "http://#{application}.rubyforge.org/svn/tags/rel_1-0-2/"
 set :deploy_via, :export
 
 set :deploy_to, "/home/dtrust/#{application}"
