d7665a957312dc5e263b3480ae6e2ea5e3800753
smithsf <smithsf@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-01-24 15:36:29 +0000

    Added Security

--- app/controllers/bus_admin/loads_controller.rb
+++ app/controllers/bus_admin/loads_controller.rb
@@ -1,5 +1,5 @@
 class BusAdmin::LoadsController < ApplicationController
-
+   before_filter :login_required, :check_authorization 
    active_scaffold :loads do |config|
     config.columns = [:name, :email, :sent, :invitation]  
  end
