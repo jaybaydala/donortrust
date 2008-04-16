4dfaaa495455ea01f98383c7fad2184026f0179b
j27lee <j27lee@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-04-16 23:32:53 +0000

    unallocated_investments_controller.rb - changed project id in code to reflect the proper "Let CF decide" project id (11)
    unallocated_investments/index.rhtml
    - clarified titling of page

--- app/controllers/bus_admin/unallocated_investments_controller.rb
+++ app/controllers/bus_admin/unallocated_investments_controller.rb
@@ -1,21 +1,21 @@
 class BusAdmin::UnallocatedInvestmentsController < ApplicationController
   before_filter :login_required, :check_authorization
   def index
-    @unallocated_investments = Investment.find(:all, :conditions => ['project_id = 1 '])
+    @unallocated_investments = Investment.find(:all, :conditions => ['project_id = 11'])
    end
-  
+
   def unallocate
-    
+
     @investmentId = params[:investment_id]
     @investment = Investment.find(@investmentId)
- 
+
     @investment.project_id = params[:projectId]
     @investment.save
-      
+
     if @investment.valid?
       flash[:notice] = 'Investment was saved.'
     else
-      flash[:notice] = @investment.errors.to_xml 
+      flash[:notice] = @investment.errors.to_xml
       break
     end
     render(:update) { |page| page.call 'location.reload' }
