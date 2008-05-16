38351234f5ebcc2ec9176f8fc66f3c5e6bac1e81
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-05-16 19:35:53 +0000

    prepping build branch

---
+++ spec/controllers/dt/checkouts_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Dt::CheckoutsController do
+
+  #Delete this example and add some real ones
+  it "should use Dt::CheckoutsController" do
+    controller.should be_an_instance_of(Dt::CheckoutsController)
+  end
+
+end
---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,43 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Cart do
+  before do
+    @cart = Cart.new
+  end
+  
+  it "should contain items" do
+    @cart.items.should == []
+  end
+  
+  it "should have a total" do
+    @cart.total.should == 0.0
+  end
+  
+  describe "items" do
+    it "should allow Gift items" do
+      @cart.add_item Gift.new
+      @cart.items.last.class.should == Gift
+    end
+    it "should allow Investment items" do
+      @cart.add_item Investment.new
+      @cart.items.last.class.should == Investment
+    end
+    it "should allow Deposit items" do
+      @cart.add_item Deposit.new
+      @cart.items.last.class.should == Deposit
+    end
+    it "should not allow Project items" do
+      @cart.add_item Project.new
+      @cart.items.last.class.should_not == Project
+    end
+  end
+  
+  it "should be empty?" do
+    @cart.empty?.should == true
+  end
+  
+  it "should not be empty? after an item is added" do
+    @cart.add_item Gift.new
+    @cart.empty?.should == false
+  end
+end
