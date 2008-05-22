91631cfd772c2e32ca8e0d533e813cccf8f54cf2
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-05-22 17:02:26 +0000

    updated staging deployment to correct branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ require 'config/environment'
 require 'capistrano/ext/multistage'
 require 'mongrel_cluster/recipes'
 set :application, "donortrust"
-set :repository,  "http://#{application}.rubyforge.org/svn/trunk/"
+set :repository,  "http://#{application}.rubyforge.org/svn/branches/1-2-working"
 
 set :mongrel_conf, "/etc/mongrel_cluster/#{application}-staging.yml"
 set :mongrel_admin_conf, "/etc/mongrel_cluster/#{application}-staging_admin.yml"
