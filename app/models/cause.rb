6780d5c2f879403ee28a6c7eab8b1cce967c885b
j27lee <j27lee@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-08-07 18:09:11 +0000

    models/cause.rb, models/sector.rb - updated models to reflect assignment of causes to sectors

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,15 +1,15 @@
 class Cause < ActiveRecord::Base
   acts_as_paranoid
-  
-  validates_presence_of :name  
+
+  validates_presence_of :name
   validates_uniqueness_of :name
-  
+
   has_many :projects
   has_many :cause_limits
   has_many :campaigns, :through => :cause_limit
   has_and_belongs_to_many :millennium_goals
-  has_and_belongs_to_many :sectors
-  
-  
+
+  belongs_to :sector
+
   acts_as_textiled :description
 end
--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,13 +1,11 @@
 class Sector < ActiveRecord::Base
   acts_as_paranoid
-  
+
   has_and_belongs_to_many :projects
-  has_and_belongs_to_many :causes
-  
-  has_many :place_sectors
-  has_many :places , :through => :place_sectors
-  has_many :quick_fact_sectors   
-     
+
+  has_many :causes
+  has_many :quick_fact_sectors
+
   validates_presence_of :name
   validates_uniqueness_of :name
 
