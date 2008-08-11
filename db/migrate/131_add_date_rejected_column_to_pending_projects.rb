83ae3c7764c1777f498d73e15eb5262a4c50ce7d
lgomes <lgomes@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-08-11 00:49:27 +0000

    Reorganizing migrations

--- db/migrate/131_add_date_rejected_column_to_pending_projects.rb
+++
@@ -1,9 +0,0 @@
-class AddDateRejectedColumnToPendingProjects < ActiveRecord::Migration
-  def self.up
-    add_column :pending_projects, :date_rejected, :date, :null => true
-  end
-
-  def self.down
-    remove_column :pending_projects, :date_rejected
-  end
-end
