511fe2c1052390c6b1f9c572aa56016b52e23fe8
j27lee <j27lee@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-08-13 08:24:01 +0000

    db/migrate/136_add_short_desc_to_projects.rb - added short description to project_versions table

--- db/migrate/136_add_short_desc_to_projects.rb
+++ db/migrate/136_add_short_desc_to_projects.rb
@@ -1,9 +1,11 @@
 class AddShortDescToProjects < ActiveRecord::Migration
   def self.up
     add_column :projects, :short_description, :string, :limit => 255
+    add_column :project_versions, :short_description, :string, :limit => 255
   end
 
   def self.down
     remove_column :projects, :short_description
+    remove_column :project_versions, :short_description
   end
 end
