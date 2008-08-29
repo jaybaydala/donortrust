893d69c5889fdb76a2cac4a3d5e4a6c44eefa0ab
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-08-29 16:58:05 +0000

    Temporary updates for 1-3-development branch deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,21 +1,44 @@
-set :mongrel_conf, "/etc/mongrel_cluster/#{application}-staging.yml"
-set :mongrel_admin_conf, "/etc/mongrel_cluster/#{application}-staging_admin.yml"
-
+# this is a configuration for Dreamhost's Passenger 
+# - it shouldn't get back into the 1-2-stable branch or trunk
 set :stage, "staging"
-set :deploy_to, "/home/dtrust/#{application}_staging"
-set :user, "dtrust"
+set :deploy_to, "/home/pivot/dt.pivotib.com/apps/#{application}"
+set :domain, "dt.pivotib.com"
+set :user, "pivot"
 set :rails_env, "staging"
-role :app, "slice2.christmasfuture.org"
-role :admin, "slice2.christmasfuture.org"
-role :web, "slice.christmasfuture.org"
-role :db,  "slice.christmasfuture.org", :primary => true
-role :schedule,  "slice2.christmasfuture.org"
+set :use_sudo, false
+role :app, domain
+role :admin, domain
+role :web, domain
+role :db, domain, :primary => true
+role :schedule, domain
 
 namespace :deploy do
-  task :after_update_code, :rols => :app do
-    run <<-CMD
-      cd #{release_path} && rake deploy_edge REVISION=#{rails_version} 
-    CMD
+  task :cold do
+    update
+    migrate
+    start
+  end
+
+  task :start,    :roles => :app do
+    # do nothing
+  end
+  task :stop,    :roles => :app do
+    # do nothing
+  end
+  task :restart,  :roles => :app do
+    run "touch {release_path}/tmp/restart.txt"
+  end
+  task :start_admin,    :roles => :app do
+    # do nothing
+  end
+  task :stop_admin,    :roles => :app do
+    # do nothing
+  end
+  task :restart_admin,  :roles => :app do
+    # do nothing
+  end
+  
+  task :after_update_code, :roles => :app do
     run <<-CMD
       mv #{release_path}/config/backgroundrb.yml.staging #{release_path}/config/backgroundrb.yml
     CMD
@@ -24,34 +47,13 @@ namespace :deploy do
     CMD
   end
   
-  desc <<-DESC
-  symlink the images, stylesheets and javascripts to current_path
-  DESC
-  task :asset_folder_fix , :roles => :web do
-    cmd = ""
-    %w( stylesheets javascripts ).each do |dir|
-      asset_path = "#{latest_release}/public/#{dir}"
-      server_path = "/var/www/staging.christmasfuture.org/#{dir}"
-      cmd += " && " unless cmd.empty?
-      cmd += "rm -f #{server_path} && ln -s #{asset_path} #{server_path}"
-    end
-    image_paths = ["active_scaffold", "bus_admin", "calendar.gif", "dt", "rails.png", "redbox_spinner.gif"]
-    image_paths.each do |image|
-      asset_path = "#{latest_release}/public/images/#{image}"
-      server_path = "/var/www/staging.christmasfuture.org/images/#{image}"
-      cmd += " && " unless cmd.empty?
-      cmd += "rm -f #{server_path} && ln -s #{asset_path} #{server_path}"
-    end
-    send(run_method, cmd)
-  end
-  
   task :start_backgroundrb , :roles => :schedule do
     # don't do anything on the staging server - backgroundrb isn't running for now
   end
   task :stop_backgroundrb , :roles => :schedule do
     # don't do anything on the staging server - backgroundrb isn't running for now
   end
-  task :restart_backgroundrb , :roles => :app do
+  task :restart_backgroundrb , :roles => :schedule do
     # don't do anything on the staging server - backgroundrb isn't running for now
   end
   
