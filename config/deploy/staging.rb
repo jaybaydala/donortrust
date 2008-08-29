04e83d96123c62d024364e39b92e4fbefcdf70eb
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-08-29 20:02:33 +0000

    Ported r2529:2532 (deployment and css fixes) from trunk

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -19,15 +19,12 @@ role :db,  "slice.christmasfuture.org", :primary => true
 role :schedule,  "slice2.christmasfuture.org"
 
 namespace :deploy do
-  task :after_update_code, :rols => :app do
+  task :after_update_code, :roles => :app do
     run <<-CMD
       cd #{release_path} && rake deploy_edge REVISION=#{rails_version} 
     CMD
     run <<-CMD
       mv #{release_path}/config/backgroundrb.yml.staging #{release_path}/config/backgroundrb.yml
-    CMD
-    run <<-CMD
-      cd #{release_path} && rake deploy_edge REVISION=#{rails_version} 
     CMD
   end
   
@@ -35,21 +32,19 @@ namespace :deploy do
   symlink the images, stylesheets and javascripts to current_path
   DESC
   task :asset_folder_fix , :roles => :web do
-    cmd = ""
     %w( stylesheets javascripts ).each do |dir|
       asset_path = "#{latest_release}/public/#{dir}"
       server_path = "/var/www/staging.christmasfuture.org/#{dir}"
-      cmd += " && " unless cmd.empty?
-      cmd += "rm -f #{server_path} && ln -s #{asset_path} #{server_path}"
+      send(run_method, "rm -f #{server_path}")
+      send(run_method, "ln -s #{asset_path} #{server_path}")
     end
     image_paths = ["active_scaffold", "bus_admin", "calendar.gif", "dt", "rails.png", "redbox_spinner.gif"]
     image_paths.each do |image|
       asset_path = "#{latest_release}/public/images/#{image}"
       server_path = "/var/www/staging.christmasfuture.org/images/#{image}"
-      cmd += " && " unless cmd.empty?
-      cmd += "rm -f #{server_path} && ln -s #{asset_path} #{server_path}"
+      send(run_method, "rm -f #{server_path}")
+      send(run_method, "ln -s #{asset_path} #{server_path}")
     end
-    send(run_method, cmd)
   end
   
   task :start_backgroundrb , :roles => :schedule do
