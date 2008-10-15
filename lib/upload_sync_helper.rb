96c696a337c655bd590a5b7b0860d535a243c913
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-10-15 13:49:53 +0000

    right - need to do a system call here :)

--- lib/upload_sync_helper.rb
+++ lib/upload_sync_helper.rb
@@ -1,6 +1,6 @@
 module UploadSyncHelper
   protected
   def sync_uploads
-    "cd #{RAILS_ROOT} && cap #{ENV['RAILS_ENV']} deploy:sync_uploads"
+    system("cd #{RAILS_ROOT} && cap #{ENV['RAILS_ENV']} deploy:sync_uploads")
   end
 end
