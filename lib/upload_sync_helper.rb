c435bd0b24d5d02c840158b95452c4a760e03b21
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-10-16 14:47:52 +0000

    limit sync_uploads to staging and production environments

--- lib/upload_sync_helper.rb
+++ lib/upload_sync_helper.rb
@@ -1,6 +1,6 @@
 module UploadSyncHelper
   protected
   def sync_uploads
-    system("cd #{RAILS_ROOT} && RAILS_ENV=#{ENV['RAILS_ENV']} script/sync_uploads")
+    system("cd #{RAILS_ROOT} && RAILS_ENV=#{ENV['RAILS_ENV']} script/sync_uploads") if %w(staging production).include?(ENV['RAILS_ENV'])
   end
 end
