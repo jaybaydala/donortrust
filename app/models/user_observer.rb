e8b32829b0cc20b65f2d5cf355c98b0deaaecb70
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-10-17 20:17:49 +0000

    Remove "activation email" after a user activates their own account

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -3,10 +3,6 @@ class UserObserver < ActiveRecord::Observer
     DonortrustMailer.deliver_user_signup_notification(user)
   end
 
-  def after_save(user)
-    DonortrustMailer.deliver_user_activation(user) if user.recently_activated?
-  end
-
   def after_update(user)
     DonortrustMailer.deliver_user_change_notification(user) if user.login_changed?
   end
