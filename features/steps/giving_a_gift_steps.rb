0dbeafadd827c2169dd30b0bbecf080034735ca4
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-10-20 20:44:51 +0000

    small fixes

--- features/steps/giving_a_gift_steps.rb
+++ features/steps/giving_a_gift_steps.rb
@@ -1,17 +1,10 @@
 Then /^I should see 3 gifts in my cart$/ do
-puts response.body
   response.should have_tag("#cart div.gift", :count => 3)
 end
 
-When /^I upload a valid file to Import$/ do
+Then /^I should see all the email addresses from the file in "Gift Recipients" PENDING$/ do
 end
 
-Then /^I should see all the email addresses from the file in "Gift Recipients"$/ do
-end
-
-When /^I upload an invalid file to Import$/ do
-end
-
-Then /^I should get an error message with further instructions$/ do
+Then /^I should get an error message with further instructions PENDING$/ do
 end
 
