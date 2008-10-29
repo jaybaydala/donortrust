921352c2a4bbf86cc8a2ce97ee91383a69015ea9
ervin <ervin@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-10-29 22:49:23 +0000

    Added bulk gifts functionality

--- app/models/email_parser.rb
+++ app/models/email_parser.rb
@@ -5,6 +5,7 @@ class EmailParser
   def initialize(file_or_data)
     @emails = []
     @errors = []
+    
     if file_or_data.is_a? String
       @data = file_or_data
     elsif file_or_data.respond_to?("read")
@@ -16,22 +17,28 @@ class EmailParser
     FasterCSV.parse(@data, { :headers => true, :return_headers => false }) do |row|
       begin
         email = TMail::Address.parse("#{row[0]} <#{row[1]}>")
-        @emails << email
       rescue TMail::SyntaxError
+        @errors << email
+      else
+          @emails << email unless @emails.detect{|e| e.address == email.address }
       end
-    end
+    end  
     @emails
   end
   
   def parse_list
     @data.split(',').each do |email|
       begin
-        email = TMail::Address.parse(email)
-        @emails << email unless @emails.detect{|e| e.address == email.address }
+        email = TMail::Address.parse(email)      
+        if valid?(email.address)
+          @emails << email unless @emails.detect{|e| e.address == email.address }
+        else
+          @errors << email.address.to_s
+        end
       rescue TMail::SyntaxError
         @errors << email.strip
       end
-    end
+    end  
     @emails
   end
 
@@ -41,5 +48,14 @@ class EmailParser
     rescue TMail::SyntaxError
       false
     end
-  end  
+  end
+  
+  private
+  def valid?(address)
+    unless address.to_s.match(/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i)
+      false
+    else
+      true
+    end
+  end
 end
