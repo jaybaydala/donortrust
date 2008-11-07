6117a8a612a382891cc2a0c88617d67dc6edf13a
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-11-07 19:49:18 +0000

    Fixed remote_link_renderer for cart checkout sidebar

--- app/helpers/remote_link_renderer.rb
+++ app/helpers/remote_link_renderer.rb
@@ -7,14 +7,14 @@ class RemoteLinkRenderer < WillPaginate::LinkRenderer
   protected
   def page_link(page, text, attributes = {})
     if @remote[:special_url] == "checkoutcart" && @remote[:url]
-      @remote.delete(:special_url)
       @remote[:url] = url_for_checkoutcart(page, @remote[:url])
     end
     @template.link_to_remote(text, {:url => url_for(page), :method => :get}.merge(@remote), :href => url_for(page))
   end
 
   def url_for_checkoutcart(page, url)
-    "#{url}#{url.match(/\?/) ? "&" : "?"}#{param_name}=#{page}"
+    url = "#{url}#{url.match(/\?/) ? "&" : "?"}#{CGI.escape param_name}=@" unless url.match(%r!(#{CGI.escape param_name}=)!)
+    url = url.sub(%r!((?:\?|&(?:amp;)?)#{CGI.escape param_name}=)\d+!, '\1@')
+    url.sub '@', page.to_s
   end
 end
-
