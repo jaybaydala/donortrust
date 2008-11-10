217c771802340cb548dcaad6f15e145418641de6
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-11-10 19:20:53 +0000

    making upgrade to rails 2.1.2 official - to use the new security patches

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 ENV['RAILS_ENV'] ||= 'development'
 
-RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 require 'fastercsv'
