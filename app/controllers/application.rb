709fcad071a86f623d7b5bd2e6f30f99b0dd4933
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-11-11 02:16:59 +0000

    refactored application.rb to simplify a little bit

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -25,45 +25,43 @@ class ApplicationController < ActionController::Base
   end
 
   def check_authorization
-    if logged_in?
-      roles = current_user.roles
-      requested_action = action_name
-      requested_controller = self.class.controller_path
-      requested_controller_id = self;
-      actions = [];
-      roles.each do |role|
-        role.authorized_actions.each do |action|
-          actions << action
-        end
+    return false unless logged_in?
+    roles = current_user.roles
+    requested_action = action_name
+    requested_controller = self.class.controller_path
+    requested_controller_id = self
+    actions = []
+    roles.each do |role|
+      role.authorized_actions.each do |action|
+        actions << action
       end
-      unless actions.detect do |action|
-          permitted_actions = AuthorizedAction.find :all, :conditions => {"authorized_controller_id","#{requested_controller_id}"}
-          for permitted_action in permitted_actions
-            if user_type.bus_secure_action_id == permitted_action.id || indirect_approve()
-              return true
-            end
-          end
-
-          #puts "Permitted action: " + action.name + " Desired Action: " + requested_action.to_s + " With controller: " + requested_controller
-          if direct_approve(requested_action.to_s, action.name.to_s, requested_controller.to_s,  action.authorized_controller.name.to_s ) ||
-              indirect_approve(requested_action.to_s, action.name.to_s, requested_controller.to_s, action.authorized_controller.name.to_s, requested_controller_id )
-            return true
-          end
-      end
-
-        flash[:notice] = "You are not authorized to view the requested page."
-
-
-        if request.parameters.has_value?('_list_inline_adapter') || request.parameters.has_value?('_method=delete')
-          render :text => "You do not have access"
-        else
-          redirect_to('/dt')
-        end
-        return false
-      end
-    else
-      return false
     end
+    unless actions.detect do |action|
+      permitted_actions = AuthorizedAction.find(:all, :conditions => {"authorized_controller_id","#{requested_controller_id}"})
+      permitted_actions.each do |permitted_action|
+        return true if user_type.bus_secure_action_id == permitted_action.id || indirect_approve()
+      end
+
+      #puts "Permitted action: " + action.name + " Desired Action: " + requested_action.to_s + " With controller: " + requested_controller
+      return approval?(requested_action.to_s, action.name.to_s, requested_controller.to_s, action.authorized_controller.name.to_s, requested_controller_id)
+    end
+
+    flash[:notice] = "You are not authorized to view the requested page."
+
+    if request.parameters.has_value?('_list_inline_adapter') || request.parameters.has_value?('_method=delete')
+      render :text => "You do not have access"
+    else
+      redirect_to('/dt')
+    end
+    return false
+  end
+  end
+  
+  def approval?(requested_action, permitted_action, requested_controller, permitted_controller, requested_controller_id)
+    approved = 
+      direct_approve(requested_action, permitted_action, requested_controller, permitted_controller) || 
+      indirect_approve(requested_action, permitted_action, requested_controller, permitted_controller, requested_controller_id)
+    approved
   end
 
   def direct_approve (requested_action, permitted_action, requested_controller, permitted_controller)
@@ -100,73 +98,72 @@ class ApplicationController < ActionController::Base
       return permitted_action == 'record_management' && (requested_controller == permitted_controller)
     else
       defined? requested_controller_id.get_local_actions(requested_action,permitted_action)
-
     end
   end
 
-protected
+  protected
 
-def permission_denied
-  flash[:notice] = "You don't have privileges to access this action"
-  return redirect_to('/dt')
-end
-
-def permission_granted
-  # CHANGED: commented the following line as it overrode all the normal flash notices!
-  # flash[:notice] = "Welcome to the business administration area!"
-end
-
-def ssl_filter
-  if ['production'].include?(ENV['RAILS_ENV'])
-    redirect_to url_for(params.merge({:protocol => 'https://'})) and return false if !request.ssl? && ssl_required?
-    redirect_to url_for(params.merge({:protocol => 'http://'})) and return false if request.ssl? && !ssl_required?
+  def permission_denied
+    flash[:notice] = "You don't have privileges to access this action"
+    return redirect_to('/dt')
   end
-end
 
-#MP - Dec 14, 2007
-#Added to support the us tax receipt functionality
-#allows us to set a value indicating that the user has requested
-#a US tax receipt. If the value is false, the session variable is
-#cleared, otherwise it is set to true
-def requires_us_tax_receipt(value)
-  if value
-    session[:requires_us_tax_receipt] = value
-  else
-    session[:requires_us_tax_receipt] = nil unless session[:requires_us_tax_receipt].nil?
+  def permission_granted
+    # CHANGED: commented the following line as it overrode all the normal flash notices!
+    # flash[:notice] = "Welcome to the business administration area!"
   end
-end
 
-#MP - Dec 14, 2007
-#Added to support the us tax receipt functionality
-#If the user has indicated that they want a US tax
-#receipt, the session variable will be true,
-#otherwise it should be nil.
-def requires_us_tax_receipt?
-  return session[:requires_us_tax_receipt] unless session[:requires_us_tax_receipt].nil?
-  false
-end
-
-def ssl_required?
-  false
-end
-
-def log_error(exception)
-  super(exception)
-  if ENV['RAILS_ENV'] == 'production'
-    begin
-      ErrorMailer.deliver_snapshot(
-        exception,
-        clean_backtrace(exception),
-        @session.instance_variable_get("@data"),
-        @params,
-        @request.env)
-    rescue => e
-      logger.error(e)
+  def ssl_filter
+    if ['production'].include?(ENV['RAILS_ENV'])
+      redirect_to url_for(params.merge({:protocol => 'https://'})) and return false if !request.ssl? && ssl_required?
+      redirect_to url_for(params.merge({:protocol => 'http://'})) and return false if request.ssl? && !ssl_required?
     end
   end
-end
 
-# Error Handling
-class DtNotFoundError < Exception
-end
+  #MP - Dec 14, 2007
+  #Added to support the us tax receipt functionality
+  #allows us to set a value indicating that the user has requested
+  #a US tax receipt. If the value is false, the session variable is
+  #cleared, otherwise it is set to true
+  def requires_us_tax_receipt(value)
+    if value
+      session[:requires_us_tax_receipt] = value
+    else
+      session[:requires_us_tax_receipt] = nil unless session[:requires_us_tax_receipt].nil?
+    end
+  end
+
+  #MP - Dec 14, 2007
+  #Added to support the us tax receipt functionality
+  #If the user has indicated that they want a US tax
+  #receipt, the session variable will be true,
+  #otherwise it should be nil.
+  def requires_us_tax_receipt?
+    return session[:requires_us_tax_receipt] unless session[:requires_us_tax_receipt].nil?
+    false
+  end
+
+  def ssl_required?
+    false
+  end
+
+  def log_error(exception)
+    super(exception)
+    if ENV['RAILS_ENV'] == 'production'
+      begin
+        ErrorMailer.deliver_snapshot(
+          exception,
+          clean_backtrace(exception),
+          @session.instance_variable_get("@data"),
+          @params,
+          @request.env)
+      rescue => e
+        logger.error(e)
+      end
+    end
+  end
+
+  # Error Handling
+  class DtNotFoundError < Exception
+  end
 end
