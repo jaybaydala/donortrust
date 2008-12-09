da207834499dbe700ed5e95eae4e2bb5baf22865
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2008-12-09 01:31:34 +0000

    Path fixes

--- app/controllers/bus_admin/projects_controller.rb
+++ app/controllers/bus_admin/projects_controller.rb
@@ -126,7 +126,7 @@ class BusAdmin::ProjectsController < ApplicationController
         end
       end
     else
-      redirect_to show_bus_admin_home_path
+      redirect_to home_path
     end
   end
 
