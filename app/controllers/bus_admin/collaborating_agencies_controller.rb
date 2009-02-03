c69dbc1a4a7585a1071a27f9565a4511a0d718de
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2009-02-03 16:35:50 +0000

    Ported r3005 (fix for bud #23340) from trunk

--- app/controllers/bus_admin/collaborating_agencies_controller.rb
+++ app/controllers/bus_admin/collaborating_agencies_controller.rb
@@ -1,10 +1,19 @@
 class BusAdmin::CollaboratingAgenciesController < ApplicationController
-  layout 'admin'
+  layout :choose_layout
   before_filter :login_required, :check_authorization
   #access_control :DEFAULT => 'cf_admin'
 
   active_scaffold :collaborating_agencies do |config|
     config.list.columns =[ :agency_name ]
     config.columns =[ :agency_name, :description ]
+  end
+
+  private
+  def choose_layout    
+    if params['page_format'] == 'admin_popup' 
+      'admin_popup'
+    else
+      'admin'
+    end
   end
 end
