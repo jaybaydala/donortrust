0ec0fe9d82761108b781e819b19fd14e1bcd0cb2
jrickard <jrickard@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2009-04-28 22:06:25 +0000

    added properties to the Report class in order to select the default value on the report selection screen

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,11 @@
-  require 'csv'
+require 'csv'
 class Report < ActiveRecord::Base
+      attr_accessor :gift, :start_date, :end_date
 
-
+      def initialize(attributes = nil)
+      	  @gift = 'gift_report'
+	  puts "Initialization run"
+      end
 end
    
    
