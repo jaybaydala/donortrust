a87dd27558516b8e3249c9aad14d86b61cc1af4a
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2009-09-08 02:46:52 +0000

    merged r3137:3139 from trunk

--- app/models/unpaid_participant.rb
+++ app/models/unpaid_participant.rb
@@ -1,4 +1,10 @@
 class UnpaidParticipant < ActiveRecord::Base
+  image_column  :picture,
+                :versions => { :thumb => "100x100", :full => "200x200"  },
+                :filename => proc{|inst, orig, ext| "participant_#{inst.id}.#{ext}"},
+                :store_dir => "uploaded_pictures/participant_pictures"
+
+  validates_size_of :picture, :maximum => 500000, :message => "might be too big, must be smaller than 500kB!", :allow_nil => true
   
   image_column  :picture,
                 :versions => { :thumb => "100x100", :full => "200x200"  },
