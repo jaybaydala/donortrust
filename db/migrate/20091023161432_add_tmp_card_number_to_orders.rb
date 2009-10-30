cd5c5a437cf8dcf6723ad1f730dedd730019c7dd
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2009-10-30 14:30:00 +0000

    don't need tmp_card_number in orders table

--- db/migrate/20091023161432_add_tmp_card_number_to_orders.rb
+++
@@ -1,9 +0,0 @@
-class AddTmpCardNumberToOrders < ActiveRecord::Migration
-  def self.up
-    add_column :orders, :tmp_card_number, :string, :length => 16
-  end
-
-  def self.down
-    remove_column :orders, :tmp_card_number
-  end
-end
