8555740f6781639827dbd10286d197a6d640dc2f
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2009-11-03 16:26:03 +0000

    fixed staging deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@ set :repository,  "http://#{application}.rubyforge.org/svn/trunk/"
 set :mongrel_conf, "/etc/mongrel_cluster/#{application}-staging.yml"
 set :mongrel_admin_conf, "/etc/mongrel_cluster/#{application}-staging_admin.yml"
 set :mongrel_clean, true
-set :mongrel_rails, "/opt/ruby-enterprise/bin/mongrel_rails"
+set :mongrel_rails, "mongrel_rails"
 
 set :stage, "staging"
 set :deploy_to, "/home/dtrust/#{application}_staging"
@@ -29,14 +29,14 @@ namespace :deploy do
   task :asset_folder_fix , :roles => :web do
     %w( stylesheets javascripts images/uploaded_pictures ).each do |dir|
       asset_path = "#{latest_release}/public/#{dir}"
-      server_path = "/var/www/staging.christmasfuture.org/#{dir}"
+      server_path = "/var/www/staging.uend.org/#{dir}"
       send(run_method, "rm -f #{server_path}")
       send(run_method, "ln -s #{asset_path} #{server_path}")
     end
     image_paths = ["active_scaffold", "bus_admin", "calendar.gif", "dt", "rails.png", "redbox_spinner.gif"]
     image_paths.each do |image|
       asset_path = "#{latest_release}/public/images/#{image}"
-      server_path = "/var/www/staging.christmasfuture.org/images/#{image}"
+      server_path = "/var/www/staging.uend.org/images/#{image}"
       send(run_method, "rm -f #{server_path}")
       send(run_method, "ln -s #{asset_path} #{server_path}")
     end
