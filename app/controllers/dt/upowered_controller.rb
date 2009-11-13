c7a5925e776aadf31f3298f7b6c19994dd689ce4
ervin <ervin@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2009-11-13 00:47:10 +0000

    Added subscribers to uPowered page

--- app/controllers/dt/upowered_controller.rb
+++ app/controllers/dt/upowered_controller.rb
@@ -4,5 +4,6 @@ class Dt::UpoweredController < DtApplicationController
     @page_sidebar = Page.find_by_permalink("upowered_sidebar")
     @progress_widgets = StatisticWidget.all(:order => :position, :conditions => ["active=?",true], :limit => 2)
     @statistic_widgets = StatisticWidget.all(:order => :position, :conditions => ["active=? AND id NOT IN(?)", true, @progress_widgets.map(&:id)])
+    @subscriptions =Subscription.all(:order => 'rand()', :limit => 57, :include => :user, :conditions => "user_id IS NOT NULL")
   end
 end
