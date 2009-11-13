ec013096a5ae570d2ec5cf7fa3986adf7f6f6f89
ervin <ervin@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2009-11-13 20:43:34 +0000

    Updated staff and subscribers section on uPowered page

--- app/controllers/dt/staff_controller.rb
+++ app/controllers/dt/staff_controller.rb
@@ -2,7 +2,22 @@ class Dt::StaffController < DtApplicationController
   # before_filter :login_required
   
   def show
-    @users = User.find(:all, :conditions =>{:staff=>1})
+    user_titles = [
+                    'Executive Director',
+                    'Manager of School Program',
+                    'Manager of Operations',
+                    'Manager of Web Development',
+                    'Director of Brand and Design',
+                    'Volunteer Coordinator',
+                    'Chartered Accountant',
+                    'Partnerships',
+                    'Executive Assistant',
+                    'Social Community Manager']
+    @staff = user_titles.inject([]) do |staff, title| 
+      user = User.find_by_title_and_staff(title, true)
+      staff << { :title => title, :user => user }
+    end
+    logger.debug(@staff.inspect);
     respond_to do |format|
       format.html { }# index.html.erb
     end
--- app/controllers/dt/upowered_controller.rb
+++ app/controllers/dt/upowered_controller.rb
@@ -4,6 +4,21 @@ class Dt::UpoweredController < DtApplicationController
     @page_sidebar = Page.find_by_permalink("upowered_sidebar")
     @progress_widgets = StatisticWidget.all(:order => :position, :conditions => ["active=?",true], :limit => 2)
     @statistic_widgets = StatisticWidget.all(:order => :position, :conditions => ["active=? AND id NOT IN(?)", true, @progress_widgets.map(&:id)])
-    @subscriptions =Subscription.all(:order => 'rand()', :limit => 57, :include => :user, :conditions => "user_id IS NOT NULL")
+    user_titles = [
+                    'Executive Director',
+                    'Manager of School Program',
+                    'Manager of Operations',
+                    'Manager of Web Development',
+                    'Director of Brand and Design',
+                    'Volunteer Coordinator',
+                    'Chartered Accountant',
+                    'Partnerships',
+                    'Executive Assistant',
+                    'Social Community Manager']
+      @staff = user_titles.inject([]) do |staff, title| 
+        user = User.find_by_title_and_staff(title, true)
+        staff << { :title => title, :user => user }
+      end  
+      @mosaic_users = Subscription.all(:order => 'rand()', :limit => 57, :include => :user, :conditions => "user_id IS NOT NULL").map(&:user)
   end
 end
