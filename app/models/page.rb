e0571429b72c92218e6efc5e75d05689c974815a
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2009-11-18 05:04:26 +0000

    fixed potential 500 error

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,8 @@
 class Page < ActiveRecord::Base
   validates_presence_of :title, :permalink, :content
   has_many :wall_posts, :as => :postable, :dependent => :destroy
+  
+  def owned?(current_user)
+    false
+  end
 end
