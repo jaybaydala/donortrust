4f966fc535b4a6f411cce80bee1e41fde64900b4
sterrym <sterrym@f56dc72b-a844-47a9-82b5-8a23821eecaf>
2010-04-24 01:20:17 +0000

    Fixed deployment script for old image references

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -38,12 +38,19 @@ namespace :deploy do
   symlink the images, system, stylesheets and javascripts to current_path
   DESC
   task :asset_folder_fix , :roles => :web do
-    %w( stylesheets system javascripts images/uploaded_pictures).each do |dir|
+    %w( stylesheets system javascripts ).each do |dir|
       asset_path = "#{latest_release}/public/#{dir}"
       server_path = "/var/www/blog.christmasfuture.org/wordpress/#{dir}"
       send(run_method, "rm -f #{server_path}")
       send(run_method, "ln -s #{asset_path} #{server_path}")
     end
+    # keep backward-compatibility for images/uploaded_pictures
+    dir = "images/uploaded_pictures"
+    server_path = "/var/www/blog.christmasfuture.org/wordpress/#{dir}"
+    asset_path = "#{latest_release}/public/system/uploaded_pictures"
+    send(run_method, "rm -f #{server_path}")
+    send(run_method, "ln -s #{asset_path} #{server_path}")
+
     image_paths = ["active_scaffold", "bus_admin", "calendar.gif", "dt", "rails.png", "redbox_spinner.gif"]
     image_paths.each do |image|
       asset_path = "#{latest_release}/public/images/#{image}"
