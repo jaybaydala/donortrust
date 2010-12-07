2e5c0b4de6937e7ea36ffebb01419a45bdce8f5e
Tim Glen <tim@tag.ca>
2010-12-07 12:49:24 -0500

    more cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,9 +27,6 @@ set :user, "ideaca"
 set :group, "users"
 
 after "deploy:update_code", "deploy:configure_stuff"
-# after "deploy:start", "deploy:start_admin"
-# after "deploy:stop", "deploy:stop_admin"
-# after "deploy:restart", "deploy:restart_admin"
 
 namespace :deploy do
 
@@ -82,58 +79,9 @@ namespace :deploy do
     sudo "chown mongrel:www-data #{mongrel_conf}"
     sudo "chmod g+w #{mongrel_conf}"
   end 
-
-  task :start_admin , :roles => :admin do
-    cmd = "#{mongrel_rails} cluster::start -C #{mongrel_admin_conf}"
-    cmd += " --clean" if mongrel_clean    
-    send(run_method, cmd)
-  end
-  task :restart_admin , :roles => :admin do
-    cmd = "#{mongrel_rails} cluster::restart -C #{mongrel_admin_conf}"
-    cmd += " --clean" if mongrel_clean    
-    send(run_method, cmd)
-  end
-  task :stop_admin , :roles => :admin do
-    cmd = "#{mongrel_rails} cluster::stop -C #{mongrel_admin_conf}"
-    cmd += " --clean" if mongrel_clean    
-    send(run_method, cmd)
-  end
 end
 
 desc <<-DESC
 Check the status of all mongrel processes
 DESC
 task :status,  :roles => :app do mongrel.cluster.status end
-after "status", "admin_status"
-task :admin_status , :roles => :admin do
-  send(run_method, "#{mongrel_rails} cluster::status -C #{mongrel_admin_conf}")
-end
-
-# THESE ARE TO HELP MOVE THE PRODUCTION DB TO YOUR DEV ENVIRONMENT
-# to use, do this: `rake db:production_data_refresh`
-desc 'Dumps, downloads and then cleans up the production data dump'
-task :remote_db_runner do
-  remote_db_dump
-  remote_db_download
-  remote_db_cleanup
-end
-
-desc 'Dumps the #{rails_env} database to db/#{rails_env}_data.sql on the remote server'
-task :remote_db_dump, :roles => :db, :only => { :primary => true } do
-  run "cd #{deploy_to}/current && " +
-    "rake RAILS_ENV=#{rails_env} db:database_dump --trace" 
-end
-
-desc 'Downloads db/#{rails_env}_data.sql from the remote environment to your local machine'
-task :remote_db_download, :roles => :db, :only => { :primary => true } do  
-  execute_on_servers(options) do |servers|
-    self.sessions[servers.first].sftp.connect do |tsftp|
-      tsftp.get_file "#{deploy_to}/current/db/#{rails_env}_data.sql", "db/production_data.sql" 
-    end
-  end
-end
-
-desc 'Cleans up data dump file'
-task :remote_db_cleanup, :roles => :db, :only => { :primary => true } do
-  run "rm -f #{deploy_to}/current/db/#{rails_env}_data.sql"
-end 
