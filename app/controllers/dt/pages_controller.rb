e691fbf2670df2c064717df27092ff139e1cce21
Tim Glen <tim@tag.ca>
2010-12-07 11:46:00 -0500

    Extended and fixed the pages functionality

--- app/controllers/dt/pages_controller.rb
+++ app/controllers/dt/pages_controller.rb
@@ -3,5 +3,6 @@ class Dt::PagesController < ApplicationController
     @page = Page.find_by_permalink(params[:id])
     @page = Page.find(params[:id]) unless @page
     redirect_to dt_upowered_path if @page.permalink == "upowered"
+    render :action => (@page.template.present? ? @page.template : 'show'), :layout => (@page.layout.present? ? @page.layout : 'dt/plain')
   end
 end
