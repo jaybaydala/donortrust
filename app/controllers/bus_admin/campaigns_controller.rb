3494633ba477b6352c9e25d60c0c9edb9120fcba
Tim Glen <tim@tag.ca>
2011-01-12 17:14:55 -0500

    Add a nice flash message to campaign closure

--- app/controllers/bus_admin/campaigns_controller.rb
+++ app/controllers/bus_admin/campaigns_controller.rb
@@ -3,7 +3,7 @@ class BusAdmin::CampaignsController < ApplicationController
   before_filter :login_required, :check_authorization
 
   active_scaffold do |config|
-    config.list.columns = [:name, :event_date]
+    config.list.columns = [:name, :event_date, :funds_raised, :funds_allocated]
     config.actions = [:create, :list, :search, :show ]
     config.action_links.add(:close, { :label => "Close", :method => :put, :crud_type => :update, :type => :record, :inline => false, :confirm => "Are you sure you want to close the campaign? This will auto-allocate any funds currently given to the campaign." })
   end
@@ -14,8 +14,14 @@ class BusAdmin::CampaignsController < ApplicationController
     # --- if the projects are totally funded from a campaign and there is excess cash it too will be deposited in the CF allocations account
     # - The UEnd / DonorTrust super-users have the ability to put these funds in the CF Allocations User account against projects by simply going thru the normal checkout process for making a donation to a project...and on the screen that they tell how to pay (from their credit card, account, etc) they have an additional option, to pay from the Allocations account. THey must put in a note to tell why this money is being put on this project (e.g. this is from the OneYoga campaign etc)
     @campaign = Campaign.find(params[:id])
-    @campaign.close!
-    render :text => @campaign.inspect
-    
+    @order = @campaign.close!
+    if @order
+      flash[:notice] = "The campaign has been closed successfully! All funds have been allocated.<br />\n"
+      flash[:notice] += @order.notes.gsub(/\n/,"<br />\n")
+    else
+      flash[:notice] = "The campaign could not be closed"
+    end
+    redirect_to :action => "index"
+    # render :text => @campaign.inspect
   end
 end
