cdea9e00886afa584eb5f47234e9e7334cff275d
Tim Glen <tim@tag.ca>
2011-02-08 15:30:42 -0500

    Make the e_card uploads work again

--- app/models/e_card.rb
+++ app/models/e_card.rb
@@ -1,14 +1,15 @@
 class ECard < ActiveRecord::Base
   after_save :make_uploads_world_readable
-  file_column :small,     :web_root => "system/uploaded_pictures/", :root_path => File.join(RAILS_ROOT, "public/system/uploaded_pictures")
-  file_column :medium,    :web_root => "system/uploaded_pictures/", :root_path => File.join(RAILS_ROOT, "public/system/uploaded_pictures")
-  file_column :large,     :web_root => "system/uploaded_pictures/", :root_path => File.join(RAILS_ROOT, "public/system/uploaded_pictures")
-  file_column :printable, :web_root => "system/uploaded_pictures/", :root_path => File.join(RAILS_ROOT, "public/system/uploaded_pictures")
+  file_column :small,     :web_root => "system/uploaded_pictures/", :root_path => Rails.root.join("public/system/uploaded_pictures")
+  file_column :medium,    :web_root => "system/uploaded_pictures/", :root_path => Rails.root.join("public/system/uploaded_pictures")
+  file_column :large,     :web_root => "system/uploaded_pictures/", :root_path => Rails.root.join("public/system/uploaded_pictures")
+  file_column :printable, :web_root => "system/uploaded_pictures/", :root_path => Rails.root.join("public/system/uploaded_pictures")
   has_many :gifts
   validates_presence_of :name
 
   private
   def make_uploads_world_readable
+    list = []
     list << self.small if self.small?
     list << self.medium if self.medium?
     list << self.large if self.large?
