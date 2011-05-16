0951bd97710d66b66d1f0b522812aa0ec21b7f6a
Tim Glen <tim@tag.ca>
2011-05-16 17:08:43 -0400

    Revert "Updated all default config and script files to 2.3.11"
    This reverts commit 8a9025fb0514fcf57f5fd54c7aff2b7da4c92ffb.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,3 @@
-# Settings specified here will take precedence over those in config/environment.rb
-
 # The test environment is used exclusively to run your application's
 # test suite.  You never need to work with it otherwise.  Remember that
 # your test database is "scratch space" for the test suite and is wiped
@@ -14,15 +12,10 @@ config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
 config.action_view.cache_template_loading            = true
 
-# Disable request forgery protection in test environment
-config.action_controller.allow_forgery_protection    = false
-
-# Tell Action Mailer not to deliver emails to the real world.
+# Tell ActionMailer not to deliver emails to the real world.
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-# Use SQL instead of Active Record's schema dumper when creating the test database.
-# This is necessary if your schema can't be completely dumped by the schema dumper,
-# like if you have constraints or database-specific column types
-# config.active_record.schema_format = :sql
+# require test-spec gem in test environment only
+# config.gem "test-spec", :version => ">= 0.9.0", :lib => 'test/spec'
