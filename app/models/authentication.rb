8ad75e012d624b4284350c9f20d267d2923292f1
Tim Glen <tim@tag.ca>
2011-06-09 14:16:56 -0400

    Added facebook and twitter scopes to authentications

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,9 +1,12 @@
 class Authentication < ActiveRecord::Base
   belongs_to :user
-  
+
   validates_presence_of :uid, :provider
   validates_uniqueness_of :uid, :scope => :provider
-  
+
+  named_scope :facebook, :conditions => { :provider => "facebook" }
+  named_scope :twitter, :conditions => { :provider => "twitter" }
+
   def provider_name
     if provider == 'open_id'
       "OpenID"
