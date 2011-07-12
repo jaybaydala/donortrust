c9749928753bb812ab5f4f95e934e948fb772ae3
Tim Glen <tim@tag.ca>
2011-07-12 15:59:36 -0400

    started building gift view

--- public/_/js/functions.js
+++ public/_/js/functions.js
@@ -6,8 +6,18 @@ $(document).ready(function (){
   $("#project_search_tools select").change(function() {
     $(this).parent().submit();
   });
-  $("#project-carousel").cycle({ pause:1, width:$("#project-carousel").width()+'px' });
-  console.log($("#project-carousel").height());
+  if ($('#project-carousel').length > 0) {
+    $("#project-carousel").cycle({ pause:1, width:$("#project-carousel").width()+'px' });
+  }
+
+  $('#e_card-selection img').click(function() {
+    $('#e_card-preview img').attr("src", $(this).attr("data_url"));
+    $("#gift_e_card_id").attr("value", $(this).attr("data_id"));
+  });
+
+  // if ($('#giftform').length > 0) {
+  //   $('#giftform>fieldset.inputs').hide();
+  // }
 });
 
 
