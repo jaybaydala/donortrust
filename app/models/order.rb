19b6d1c9c7235ab1a9b9d9c04daa229f6e8db9e8
Tim Glen <tim@tag.ca>
2011-07-29 00:36:11 -0400

    formalized cart<->order relationship. Changed how orders can be validated

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,9 @@ require 'active_merchant'
 require 'iats/gateways/iats'
 ActiveMerchant::Billing::Base.mode = RAILS_ENV == "production" ? :production : :test
 class Order < ActiveRecord::Base
-  validates_uniqueness_of :order_number
-
-  belongs_to :user
+  belongs_to :cart
   belongs_to :subscription
+  belongs_to :user
   has_many :investments
   has_many :gifts
   has_many :deposits
@@ -14,24 +13,88 @@ class Order < ActiveRecord::Base
   has_one :registration_fee
   has_one :tax_receipt
 
+  validates_presence_of :cart
+  validates_presence_of :donor_type,  :if => :billing_info_required?, :unless => :payment_options_step
+  validates_presence_of :first_name,  :if => lambda {|r| r.billing_info_required? && (r.cart.subscription? || r.personal_donor?) }, :unless => :payment_options_step
+  validates_presence_of :last_name,   :if => lambda {|r| r.billing_info_required? && (r.cart.subscription? || r.personal_donor?) }, :unless => :payment_options_step
+  validates_presence_of :company,     :if => lambda {|r| r.billing_info_required? && r.corporate_donor? }, :unless => :payment_options_step
+  validates_presence_of :address,     :if => :billing_info_required?, :unless => :payment_options_step
+  validates_presence_of :city,        :if => :billing_info_required?, :unless => :payment_options_step
+  validates_presence_of :postal_code, :if => :billing_info_required?, :unless => :payment_options_step
+  validates_presence_of :province,    :if => :billing_info_required?, :unless => :payment_options_step
+  validates_presence_of :country,     :if => :billing_info_required?, :unless => :payment_options_step
+  validates_presence_of :email, :unless => :payment_options_step
+  validates_format_of   :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :allow_blank => true
+  validates_uniqueness_of :order_number
+  validates_numericality_of :account_balance_payment, :allow_nil => true
+  validates_numericality_of :credit_card_payment, :allow_nil => true
+  validates_numericality_of :gift_card_payment, :allow_nil => true
+  validates_numericality_of :offline_fund_payment, :allow_nil => true
+  validates_numericality_of :pledge_account_payment, :allow_nil => true
+  validate do |order|
+    if order.payment_options_step
+      order.validate_payment
+    end
+    if order.account_signup_step
+      if self.cart.subscription? || tax_receipt_needed?
+        unless self.user
+          user = build_user_from_order
+          user.errors.full_messages.each{|msg| self.errors.add_to_base(msg) } unless user.valid?
+        end
+      end
+    end
+    if order.credit_card_step
+      order.validate_credit_card
+    end
+  end
+
   before_create :generate_order_number
   after_save :update_user_information
 
+  attr_accessor :payment_options_step, :billing_step, :account_signup_step, :credit_card_step, :receipt_step
+
   named_scope :complete, :conditions => { :complete => true }
+
+  def validate_credit_card
+    if credit_card_payment?
+      unless credit_card.valid?
+        credit_card_messages = credit_card.errors.full_messages.collect{|msg| " - #{msg}"}
+        errors.add_to_base("Your credit card information does not appear to be valid. Please correct it and try again:#{credit_card_messages.join}") 
+      end
+    end
+  end
+
+  def validate_payment
+    # validations for available balances
+    errors.add(:account_balance_payment, "cannot be more than your current account balance") if @account_balance && @account_balance > 0 && account_balance_payment? && account_balance_payment > @account_balance
+    errors.add(:gift_card_payment, "cannot be more than your current gift card balance") if @gift_card_balance && @gift_card_balance > 0 && gift_card_payment? && gift_card_payment > @gift_card_balance
+    errors.add(:pledge_account_payment, "cannot be more than your current pledge account balance") if @pledge_account_balance && @pledge_account_balance > 0 && pledge_account_payment? && pledge_account_payment > @pledge_account_balance
+    # check validity of the basic numbers
+    errors.add(:gift_card_payment, "must be a positive number") if self.gift_card_payment? && self.gift_card_payment < 0
+    errors.add(:account_balance_payment, "must be a positive number") if self.account_balance_payment? && self.account_balance_payment < 0
+    errors.add(:pledge_account_payment, "must be a positive number") if self.pledge_account_payment? && self.pledge_account_payment < 0
+    errors.add(:credit_card_payment, "must be a positive number") if self.credit_card_payment? && self.credit_card_payment < 0
+    errors.add(:offline_fund_payment, "must be a positive number") if self.offline_fund_payment? && self.offline_fund_payment < 0
+    # check validity of totals
+    errors.add_to_base("Please ensure you're paying the full amount.") if total_payments < total
+    errors.add_to_base("You only need to pay the cart total.") if total_payments > total
+    errors.add_to_base("You must pay at least #{number_to_currency(minimum_credit_payment)} from a credit card and/or gift card.") if minimum_credit_payment && minimum_credit_payment > credit_payments
+    errors.empty?
+  end
 
   def initialize(params = nil)
     super
     self.donor_type ||= self.class.personal_donor
     self.country ||= "Canada"
   end
-  
+
   def self.personal_donor
     "personal"
   end
   def self.corporate_donor
     "corporate"
   end
-  
+
   def card_number_concealed
     "**** **** **** #{card_number.to_s.rjust(4, " ")[-4, 4].strip}"
   end
@@ -66,6 +129,14 @@ class Order < ActiveRecord::Base
     user
   end
 
+  def corporate_donor?
+    self.donor_type == self.class.corporate_donor
+  end
+
+  def personal_donor?
+    self.donor_type == self.class.personal_donor
+  end
+
   # card number temporarily held in tmp_card_number
   attr_accessor :full_card_number
   def card_number=(number)
@@ -75,13 +146,13 @@ class Order < ActiveRecord::Base
     # this is a bit fancy schmancy - just so we can test with "1" for the card_number
     write_attribute(:card_number, number.to_s.rjust(4, " ")[-4, 4].strip) if number # loads it back up if it's not
   end
-  
+
   def card_number
     # return self.tmp_card_number if self.tmp_card_number?
     return @full_card_number if @full_card_number
     read_attribute(:card_number)
   end
-  
+
   # set some accessor methods
   def account_balance_payment=(val)
     write_attribute(:account_balance_payment, strip_dollar_sign(val))
@@ -120,40 +191,6 @@ class Order < ActiveRecord::Base
     "#{expiry_month.to_s.rjust(2, "0")}/#{expiry_year}"
   end
   
-  def validate_billing(cart)
-    cart_items = cart.items
-    if cart.subscription? || tax_receipt_needed?
-      unless self.user
-        user = build_user_from_order
-        user.errors.full_messages.each{|msg| self.errors.add_to_base(msg) } unless user.valid?
-      end
-      required_fields = %w(donor_type address city postal_code province country email)
-      if cart.subscription?
-        required_fields << "first_name"
-        required_fields << "last_name"
-      else
-        if self.donor_type? && self.donor_type == self.class.corporate_donor
-          required_fields << "company"
-        else
-          required_fields << "first_name"
-          required_fields << "last_name"
-        end
-      end
-      errors.add_on_blank(required_fields)
-    end
-    if self.email? && !errors.on(:email)
-      errors.add(:email, "isn't a valid email address") unless self.email =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-    end
-    # credit_card_payment
-    if credit_card_payment?
-      unless credit_card.valid?
-        credit_card_messages = credit_card.errors.full_messages.collect{|msg| " - #{msg}"}
-        errors.add_to_base("Your credit card information does not appear to be valid. Please correct it and try again:#{credit_card_messages.join}") 
-      end
-    end
-    errors.empty?
-  end
-  
   attr_accessor :account_balance, :gift_card_balance, :pledge_account_balance
   def account_balance=(val)
     @account_balance = BigDecimal.new(val.to_s)
@@ -164,26 +201,9 @@ class Order < ActiveRecord::Base
   def pledge_account_balance=(val)
     @pledge_account_balance = BigDecimal.new(val.to_s)
   end
-  def validate_payment(cart)
-    cart_items = cart.items
-    # validations for available balances
-    errors.add(:account_balance_payment, "cannot be more than your current account balance") if @account_balance && @account_balance > 0 && account_balance_payment? && account_balance_payment > @account_balance
-    errors.add(:gift_card_payment, "cannot be more than your current gift card balance") if @gift_card_balance && @gift_card_balance > 0 && gift_card_payment? && gift_card_payment > @gift_card_balance
-    errors.add(:pledge_account_payment, "cannot be more than your current pledge account balance") if @pledge_account_balance && @pledge_account_balance > 0 && pledge_account_payment? && pledge_account_payment > @pledge_account_balance
-    # check validity of the basic numbers
-    errors.add(:gift_card_payment, "must be a positive number") if self.gift_card_payment? && self.gift_card_payment < 0
-    errors.add(:account_balance_payment, "must be a positive number") if self.account_balance_payment? && self.account_balance_payment < 0
-    errors.add(:pledge_account_payment, "must be a positive number") if self.pledge_account_payment? && self.pledge_account_payment < 0
-    errors.add(:credit_card_payment, "must be a positive number") if self.credit_card_payment? && self.credit_card_payment < 0
-    errors.add(:offline_fund_payment, "must be a positive number") if self.offline_fund_payment? && self.offline_fund_payment < 0
-    # check validity of totals
-    errors.add_to_base("Please ensure you're paying the full amount.") if total_payments < total
-    errors.add_to_base("You only need to pay the cart total.") if total_payments > total
-    errors.add_to_base("You must pay at least #{number_to_currency(minimum_credit_payment(cart_items))} from a credit card and/or gift card.") if minimum_credit_payment(cart_items) && minimum_credit_payment(cart_items) > credit_payments
-    errors.empty?
-  end
 
-  def minimum_credit_payment(cart_items)
+  def minimum_credit_payment
+    cart_items = self.cart.items
     if (account_balance && account_balance > 0) || 
         (user_id? && user && user.balance > 0) || 
         (user_id? && user.pledge_accounts && user.pledge_accounts.inject(0){|sum,pa| sum+=pa.balance} > 0)
@@ -194,14 +214,14 @@ class Order < ActiveRecord::Base
     @minimum_credit_payment -= offline_fund_payment if offline_fund_payment?
     @minimum_credit_payment
   end
-  
-  def validate_confirmation(cart)
+
+  def validate_confirmation
     # run through everything just to make sure...
-    validate_payment(cart)
-    validate_billing(cart)
+    validate_payment
+    validate_billing
     errors.empty?
   end
-  
+
   def run_transaction
     logger.debug("Entering run_transaction")
     if credit_card.valid?
@@ -233,7 +253,23 @@ class Order < ActiveRecord::Base
       raise ActiveMerchant::Billing::Error.new("There was an error with the credit card.")
     end
   end
-  
+
+  def billing_address
+    {
+    :first_name   => self.first_name,
+    :last_name    => self.last_name,
+    :address      => self.address,
+    :city         => self.city,
+    :state        => self.province,
+    :zip          => self.postal_code,
+    :country      => self.country
+    }
+  end
+
+  def billing_info_required?
+    self.cart.subscription? || tax_receipt_requested?
+  end
+
   def credit_card(use_iats=true)
     unless @credit_card
       # if we're using IATS gateway, set the currency to CAD
@@ -251,17 +287,6 @@ class Order < ActiveRecord::Base
       )
     end
     @credit_card
-  end
-  def billing_address
-    {
-    :first_name   => self.first_name,
-    :last_name    => self.last_name,
-    :address      => self.address,
-    :city         => self.city,
-    :state        => self.province,
-    :zip          => self.postal_code,
-    :country      => self.country
-    }
   end
 
   def create_tax_receipt_from_order
@@ -349,6 +374,7 @@ class Order < ActiveRecord::Base
   end
 
   protected
+  
     def credit_payments
       total = BigDecimal.new("0")
       total += credit_card_payment if credit_card_payment?
