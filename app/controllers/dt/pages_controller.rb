4226935de30b015382db542dfbeb4a79d8a5d14f
Tim Glen <tim@tag.ca>
2011-08-19 15:34:16 -0400

    Made the page model into a nested set, complete with a tree view

--- app/controllers/dt/pages_controller.rb
+++ app/controllers/dt/pages_controller.rb
@@ -1,8 +1,17 @@
 class Dt::PagesController < ApplicationController
   def show
     @page = Page.find_by_permalink(params[:id])
+    find_page_by_path if params[:path].present?
     @page = Page.find(params[:id]) unless @page
     redirect_to dt_upowered_path if @page.permalink == "upowered"
-    render :action => (@page.template.present? ? @page.template : 'show'), :layout => (@page.layout.present? ? @page.layout : 'dt/plain')
+    render :action => (@page.template.present? ? @page.template : 'show'), :layout => (@page.layout.present? ? @page.layout : 'application')
   end
+
+  protected
+    def find_page_by_path
+      @page = Page.find_by_permalink(params[:path].pop)
+      params[:path].each do |permalink|
+        @page = nil unless @page.is_descendant_of?(Page.find_by_permalink(permalink))
+      end
+    end
 end
