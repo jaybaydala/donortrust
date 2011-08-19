10e4c76afa6b557fbe20e49c3e0ce3b512e47e8c
Tim Glen <tim@tag.ca>
2011-08-19 15:34:38 -0400

    missed the migration

---
+++ db/migrate/20110819150326_add_nested_set_fields_to_pages.rb
@@ -0,0 +1,13 @@
+class AddNestedSetFieldsToPages < ActiveRecord::Migration
+  def self.up
+    add_column :pages, :parent_id, :integer
+    add_column :pages, :lft, :integer
+    add_column :pages, :rgt, :integer
+  end
+
+  def self.down
+    remove_column :pages, :rgt
+    remove_column :pages, :lft
+    remove_column :pages, :parent_id
+  end
+end
