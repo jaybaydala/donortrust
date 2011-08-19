0621d463419db2dcc93922e0e3917e43b63118d3
Tim Glen <tim@tag.ca>
2011-08-18 23:26:58 -0400

    Added google authentication. Linked up the navigations

--- app/controllers/iend/iend_controller.rb
+++
@@ -1,8 +0,0 @@
-class Dt::IendController < DtApplicationController
-  def index
-  end
-  
-  def show
-    @user = User.find(params[:id])
-  end
-end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,13 @@
 OmniAuth.config.path_prefix = '/dt/auth'
 
 omniauth_config = YAML.load_file(Rails.root.join('config', 'omniauth.yml'))
+
+# require 'omniauth'
+require 'openid/store/filesystem'
 ActionController::Dispatcher.middleware.use OmniAuth::Builder do
   provider(:facebook, omniauth_config['facebook']['app_id'], omniauth_config['facebook']['app_secret'], {:scope => omniauth_config['facebook']['scope']}) if omniauth_config['facebook'].present?
   provider(:twitter, omniauth_config['twitter']['consumer_key'], omniauth_config['twitter']['consumer_secret']) if omniauth_config['twitter'].present?
   provider(:linked_in, omniauth_config['linked_in']['consumer_key'], omniauth_config['linked_in']['consumer_secret']) if omniauth_config['linked_in'].present?
+  # dedicated openid
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
 end
