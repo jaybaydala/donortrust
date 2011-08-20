81e8d04bceae4d7f3d1b0aef8beef0eba37891e4
Tim Glen <tim@tag.ca>
2011-08-20 12:56:56 -0400

    Add some necessaries to existing migrations. Seed the page data

--- db/migrate/20110819210317_add_path_to_pages.rb
+++ db/migrate/20110819210317_add_path_to_pages.rb
@@ -1,6 +1,9 @@
 class AddPathToPages < ActiveRecord::Migration
   def self.up
     add_column :pages, :path, :string
+    Page.reset_column_information
+    # load the paths
+    Page.all.each(&:save)
   end
 
   def self.down
---
+++ db/migrate/20110820131232_seed_pages.rb
@@ -0,0 +1,13 @@
+class SeedPages < ActiveRecord::Migration
+  def self.up
+    Page.rebuild!
+    require Rails.root.join('db', 'seeds', 'about_us_pages')
+    require Rails.root.join('db', 'seeds', 'get_involved_pages')
+    require Rails.root.join('db', 'seeds', 'general_pages')
+  end
+
+  def self.down
+    Page.delete_all
+    # raise ActiveRecord::IrreversibleMigration
+  end
+end
