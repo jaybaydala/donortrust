a34d902ed6c652b3a6d6d4868a449d65f7034670
Tim Glen <tim@tag.ca>
2011-08-22 21:39:03 -0400

    Added tax receipts to iend profile

--- app/controllers/iend/orders_controller.rb
+++ app/controllers/iend/orders_controller.rb
@@ -2,6 +2,6 @@ class Iend::OrdersController < DtApplicationController
   before_filter :login_required
 
   def index
-    @orders = current_user.orders.complete.paginate(:page => params[:tx_page], :order => "updated_at DESC")
+    @orders = current_user.orders.complete.paginate(:page => params[:page], :order => "updated_at DESC")
   end
 end
