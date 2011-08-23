cfa9c6d4dbd94f6f033d8089f8885bd040f489fb
Tim Glen <tim@tag.ca>
2011-08-23 07:51:46 -0400

    require login

--- app/controllers/iend/tax_receipts_controller.rb
+++ app/controllers/iend/tax_receipts_controller.rb
@@ -1,4 +1,6 @@
 class Iend::TaxReceiptsController < DtApplicationController
+  before_filter :login_required
+
   def index
     @tax_receipts = current_user.tax_receipts.paginate(:page => params[:page], :order => "updated_at DESC")
   end
