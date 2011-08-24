1a8647b0d46e9125f158fb5db4dd7dfe2f2c7b54
Tim Glen <tim@tag.ca>
2011-08-24 09:43:59 -0400

    changed snippets to not include html

--- db/migrate/20110819150326_add_nested_set_fields_to_pages.rb
+++ db/migrate/20110819150326_add_nested_set_fields_to_pages.rb
@@ -3,11 +3,13 @@ class AddNestedSetFieldsToPages < ActiveRecord::Migration
     add_column :pages, :parent_id, :integer
     add_column :pages, :lft, :integer
     add_column :pages, :rgt, :integer
+    add_column :pages, :path, :string
     Page.reset_column_information
     Page.rebuild!
   end
 
   def self.down
+    remove_column :pages, :path
     remove_column :pages, :rgt
     remove_column :pages, :lft
     remove_column :pages, :parent_id
