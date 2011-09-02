80f82b4a2f626662e76f03f6f438ac73c9b9b698
Tim Glen <tim@tag.ca>
2011-09-02 11:45:39 -0400

    tweaks

--- app/controllers/bus_admin/uploads_controller.rb
+++ app/controllers/bus_admin/uploads_controller.rb
@@ -3,12 +3,9 @@ class BusAdmin::UploadsController < ApplicationController
   before_filter :login_required, :check_authorization
 
   active_scaffold :uploads do |config|
-    config.label = "Contacts"
     config.list.columns = [:title, :file]
     config.update.columns = [:title, :file]
     config.create.columns = [:title, :file]
     config.show.columns = [:title, :file]
-
-    # config.columns[:file].form_ui = :file
   end
 end
