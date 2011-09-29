56c4e87dc089c39382282495aa81eebfa48a9064
Tim Glen <tim@tag.ca>
2011-09-29 15:26:21 -0400

    Updated to use new interface

--- features/step_definitions/order_steps.rb
+++ features/step_definitions/order_steps.rb
@@ -13,24 +13,27 @@ end
 Given /^I have completed the checkout process$/ do
   steps %Q{
     Given I am on the dt cart page
-    And I follow "Checkout"
-    And I press "Proceed to Step 2"
-    And I fill in "First Name" with "John"
-    And I fill in "Last Name" with "Smith"
-    And I fill in "Street Address" with "123 Avenue Road"
+    When I follow "Checkout"
+    And I choose "no thanks" 
+    And I press "next"
+    And I uncheck "Please provide me with a tax receipt"
+    And I fill in "First name" with "John"
+    And I fill in "Last name" with "Smith"
+    And I fill in "Street address" with "123 Avenue Road"
     And I fill in "City" with "Calgary"
     And I fill in "Province/State" with "AB"
-    And I fill in "Postal/Zip Code" with "T2Y 3N2"
+    And I fill in "Postal/Zip code" with "T2Y 3N2"
     And I select "Canada" from "Country"
     And I fill in "Email" with "john.smith@example.com"
-    And I uncheck "Please provide me with a tax receipt"
-    And I fill in "Credit Card Number" with "1"
-    And I fill in "Card Security Number (CVV)" with "989"
+    And I press "next"
+    # this is the login page
+    And I press "next"
+    And I fill in "Credit card number" with "1"
+    And I fill in "Card security number (CVV)" with "989"
     And I select "01" from "order_expiry_month"
     And I select "2018" from "order_expiry_year"
-    And I fill in "Cardholder Name" with "Jonathan Smith"
-    And I press "Proceed to Step 3"
-    And I press "Complete Checkout"
+    And I fill in "Cardholder name" with "Jonathan Smith"
+    And I press "finish"
     Then I should be on the order confirmation page
   }
 end
