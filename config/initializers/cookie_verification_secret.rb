5978dcd16636dca111fe13fe3b6817b8911002a5
Tim Glen <tim@tag.ca>
2011-10-03 15:48:00 -0400

    Make this branch identical to staging

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'efdcd560f310b6cc7bc778aaeb929c9487e9ac270b52e1a0e6f5004acef72d8d2616301237c3191554a91cd7b257f36d9016a161a021d0645fa38d8dee7dad09';
