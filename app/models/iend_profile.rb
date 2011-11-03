823f2ecf13bc02f5761323f78648dd301eb36370
Kenzie Campbell <kenzie@route19.com>
2011-11-03 10:40:59 -0300

    Added city and province to searchable profile info.

--- app/models/iend_profile.rb
+++ app/models/iend_profile.rb
@@ -2,11 +2,20 @@ class IendProfile < ActiveRecord::Base
   belongs_to :user
 
   define_index do
-    indexes [user(:first_name), user(:last_name), user(:country)], :as => :user
+    indexes [user(:first_name), user(:last_name)], :as => :user_name
+    indexes [user(:country), user(:province), user(:city)], :as => :user_location
 
     has user_id
     has created_at
     has updated_at
+  end
+
+  def display_name
+    [formatted_name, formatted_location].compact.delete_if{ |x| x.empty? }.join(', ')
+  end
+
+  def formatted_location
+    return [user.city, user.province, user.country].compact.delete_if{ |x| x.empty? }.join(', ') if location?
   end
 
   def formatted_name
