1018c2c72fbb7852521f28f8b44c29a922bd4d23
Tim Glen <tim@tag.ca>
2011-11-18 12:30:04 -0500

    only send the email when it updates

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,8 +4,9 @@ class Friendship < ActiveRecord::Base
   belongs_to :friend, :class_name => 'User' 
 
   def accept
-    update_attributes(:status => true, :accepted_at => Time.now)
-    DonortrustMailer.deliver_friendship_acceptance_email(self)
+    if update_attributes(:status => true, :accepted_at => Time.now)
+      DonortrustMailer.deliver_friendship_acceptance_email(self)
+    end
   end
 
   def accepted?
