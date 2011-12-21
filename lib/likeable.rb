dcdf1873eab2f79049ceba31a84f42c8cb95bae8
Jigar Patel <jagira@gmail.com>
2011-12-21 15:55:40 +0530

    update likeable module

--- lib/likeable.rb
+++ lib/likeable.rb
@@ -6,16 +6,18 @@ module Likeable
 
   # network :- facebook, google, local
   def like(network, user)
-    if network == 'local' || !user.nil?
+    unless (network == 'local' && user.nil?)
       user_id = user.nil? ? nil : user.id
       likes.create(:network => network, :user_id => user_id) unless liked_by?(user, network)
     end
   end
 
+  # returns likes count
   def likes_count
     likes.count
   end
 
+  # handle facebook and google plus unlikes 
   def unlike(network)
     like = likes.find(:last, :conditions => {:network => network})
     like.destroy if like
