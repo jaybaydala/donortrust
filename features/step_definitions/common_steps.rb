d850b816fb9b29d3a32fcadcb7b9759f827befb8
Tim Glen <tim@tag.ca>
2012-01-16 17:31:09 -0500

    better project loading - we can't use instance variables

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,10 +1,22 @@
-Before do
-  Project.unallocated_project || Factory.build(:unallocated_project).save
-  Project.admin_project || Factory.build(:admin_project).save
-  PartnerStatus.active || Factory.build(:partner_status, :name => "Active").save
+module CommonDataHelpers
+  def load_projects
+    Factory.build(:unallocated_project).save! unless Project.find_by_slug("unallocated").present?
+    Factory.build(:admin_project).save! unless Project.find_by_slug("admin").present?
+    Factory.build(:partner_status, :name => "Active").save! unless PartnerStatus.active.present?
+  end
+
+  def load_locations
+    %w{Continent Country City}.each{|t| PlaceType.find_by_name(t) || Factory(:place_type, :name => t) }
+    ['Canada', 'United States of America'].each {|t| Place.find_by_name_and_place_type_id(t, PlaceType.country.id) || Factory(:place, :name => t, :place_type_id => PlaceType.country.id) }
+  end
+
+  def load_common_data
+    load_projects
+    load_locations
+  end
 end
+World(CommonDataHelpers)
 
 Before do
-  %w{Continent Country City}.each{|t| Factory(:place_type, :name => t) }
-  ['Canada', 'United States of America'].each {|t| Factory(:place, :name => t, :place_type_id => PlaceType.country.id) }
+  load_common_data
 end
