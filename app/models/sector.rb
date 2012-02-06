6b198cf69eabc52a612fb184e66440670297fe92
Jigar Patel <jagira@gmail.com>
2012-02-06 12:03:23 +0530

    add sector to search results

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -37,4 +37,32 @@ class Sector < ActiveRecord::Base
   def projects
     return Project.find_public(:all, :joins => [:sectors], :conditions => ["sectors.id=#{self.id}"])
   end
+
+  def lives_affected
+    sum = 0
+    Project.find_public(:all, :joins => [:sectors], :conditions => ["sectors.id=#{self.id}"]).each {|project| sum = sum + project.lives_affected if project.lives_affected?}
+    sum
+  end
+
+  def lives_affected?
+    self.lives_affected > 0
+  end
+
+  def total_cost
+    sum = 0
+    Project.find_public(:all, :joins => [:sectors], :conditions => ["sectors.id=#{self.id}"]).each {|project| sum = sum + project.total_cost}
+    sum
+  end
+
+  def total_money_raised
+    sum = 0
+    Project.find_public(:all, :joins => [:sectors], :conditions => ["sectors.id=#{self.id}"]).each {|project| sum = sum + project.dollars_raised}
+    sum
+  end
+
+  def get_percent_raised
+    percentage = total_money_raised * 100 / total_cost
+    percentage = 100 if percentage > 100
+    percentage
+  end
 end
