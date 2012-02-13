918164543d747cc85f0ba42ffcdb50b2f2dd8fec
Jigar Patel <jagira@gmail.com>
2012-02-13 21:11:33 +0530

    deliver new subscription notification to jay.baydala@uend.org

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -4,7 +4,6 @@ class UserObserver < ActiveRecord::Observer
     # See Bug #23790 in rubyforge; users no longer have to activate themselves 
     # by clicking on a link in an email  
     #DonortrustMailer.deliver_user_signup_notification(user)
-    DonortrustMailer.deliver_new_user_notification(user)
 
   end
 
