32047d68d056b749ae6c744e711c3d8af05c674b
Tim Glen <tim@tag.ca>
2012-06-15 16:09:29 -0400

    do not allow commas in email addresses

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,7 +1,7 @@
 class Feedback < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :name, :email, :subject, :message
-  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "isn't a valid email address"
+  validates_format_of :email, :with => /^([^@,\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "isn't a valid email address"
 
   after_create :send_email
 
