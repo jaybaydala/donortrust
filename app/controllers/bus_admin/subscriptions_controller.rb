878bafda06599e26497c76d798d3af35dc64d1b8
Tim Glen <tim@tag.ca>
2012-10-17 10:00:31 -0400

    Added a manual subscription run

--- app/controllers/bus_admin/subscriptions_controller.rb
+++ app/controllers/bus_admin/subscriptions_controller.rb
@@ -1,10 +1,21 @@
 require 'faster_csv'
 
 class BusAdmin::SubscriptionsController < ApplicationController
+  active_scaffold do |config|
+    config.action_links.add(:run_subscription, { :label => "Run NOW", :method => :post, :type => :record, :inline => false })
+  end
+
   def index
     @subscriptions = Subscription.all
     respond_to do |format|
       format.csv { render :action => "index", :layout => false}
     end
+  end
+
+  def run_subscription
+    @subscription = Subscription.find(params[:id])
+    order = @subscription.process_payment
+    flash[:notice] = order.inspect.html_safe
+    redirect_to bus_admin_subscriptions_path
   end
 end
