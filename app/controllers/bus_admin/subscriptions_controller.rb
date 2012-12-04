da6315c7e3d24fa9723be25158d14487d355c2a2
Tim Glen <tim@tag.ca>
2012-12-04 12:00:34 -0500

    include ID in subscription csv downloads

--- app/controllers/bus_admin/subscriptions_controller.rb
+++ app/controllers/bus_admin/subscriptions_controller.rb
@@ -4,9 +4,9 @@ class BusAdmin::SubscriptionsController < ApplicationController
   layout 'admin'
 
   active_scaffold do |config|
-    config.list.columns = [:first_name, :last_name, :email, :customer_code, :amount, :begin_date, :end_date]
+    config.list.columns = [:id, :first_name, :last_name, :email, :customer_code, :amount, :begin_date, :end_date]
     config.action_links.add(:run_subscription, { :label => "Run NOW", :method => :post, :type => :record, :inline => false })
-    config.action_links.add(:download_csv, { :label => "Download CSV", :method => :get, :inline => false })
+    config.action_links.add(:download_csv, { :label => "Download CSV", :method => :get, :type => :table, :inline => false })
   end
 
   def download_csv
