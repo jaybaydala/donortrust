09f2d9532a720b68473ac1a7e6c1b6ed16f4c3f6
Tim Glen <tim@tag.ca>
2013-11-05 12:29:04 -0500

    Removed/commented everything to do with card and taking payments

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,10 +9,6 @@ set :output, 'log/cron.log'
 # We're using some weird times in here just to keep from collisions.
 # The server can probably handle multiple of these at a time, but it doesn't hurt to spread it out either
 
-every 15.minutes do
-  rake "scheduler:gift_mailer"
-end
-
 %w(42 22 2).each do |at|
   every 1.hour, :at => at.to_i  do
     rake "scheduler:session_cleaner"
@@ -24,25 +20,29 @@ every 30.minutes do
 end
 
 every 1.month do
-  runner 'Subscription.notify_impending_card_expirations'
+  # runner 'Subscription.notify_impending_card_expirations'
   runner 'Geolocation.expire_cache'
 end
 
-every 1.day, :at => '3:50am' do
-  rake "scheduler:process_stale_accounts"
-end
+# every 15.minutes do
+#   rake "scheduler:gift_mailer"
+# end
 
-every 1.day, :at => '6:50am' do
-  rake "scheduler:account_reminder"
-end
+# every 1.day, :at => '3:50am' do
+#   rake "scheduler:process_stale_accounts"
+# end
 
-every 1.day, :at => '7:18am' do
-  rake "scheduler:gift_reminder"
-end
+# every 1.day, :at => '6:50am' do
+#   rake "scheduler:account_reminder"
+# end
 
-every 1.day, :at => '4:00am' do
-  rake "subscriptions:process_daily"
-end
+# every 1.day, :at => '7:18am' do
+#   rake "scheduler:gift_reminder"
+# end
+
+# every 1.day, :at => '4:00am' do
+#   rake "subscriptions:process_daily"
+# end
 
 # every January 4 @ 6:20am
 every '20 6 4 1 *' do
